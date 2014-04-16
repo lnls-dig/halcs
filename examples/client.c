a68677123de782670177b72f1ee3666db6eacee6
Lucas Russo <lerwys@gmail.com>
2014-04-16 15:44:24 -0300

    examples/client.c: add minimalistic command-line options
    Now, we have the option of setting the broker endpoint
    on command-line with the -b option

--- examples/client.c
+++ examples/client.c
@@ -4,9 +4,12 @@
  */
 
 #include <mdp.h>
+#include <czmq.h>
 #include <inttypes.h>
 
-#define LEDS_OPERATION                  0
+#define DFLT_BIND_FOLDER            "/tmp/bpm"
+#define DFLT_BIND_ADDR              "0"
+#define LEDS_OPERATION              0
 
 /* Our tructure */
 typedef struct _bpm_client_t {
@@ -46,12 +49,55 @@ int bpm_blink_leds (bpm_client_t *self, char *service, uint32_t leds)
     return 0;
 }
 
+void print_help (char *program_name)
+{
+    printf( "Usage: %s [options]\n"
+            "\t-h This help message\n"
+            "\t-v Verbose output\n"
+            "\t-b <broker_endpoint> Broker endpoint\n", program_name);
+}
+
 int main (int argc, char *argv [])
 {
-    int verbose = (argc > 1 && streq (argv [1], "-v"));
-    bpm_client_t *bpm_client = bpm_client_new ("ipc:///tmp/bpm/0", verbose);
+    int verbose = 0;
+    char *broker_endp = NULL;
+    char **str_p;
 
-    unsigned i;
+    if (argc < 2) {
+        print_help (argv[0]);
+        exit (1);
+    }
+
+    /* FIXME: This is rather buggy! */
+    /* Simple handling of command-line options. This should be done
+     * with getopt, for instance*/
+    int i;
+    for (i = 1; i < argc; i++)
+    {
+        if (streq(argv[i], "-v")) {
+            verbose = 1;
+        }
+        else if (streq(argv[i], "-h"))
+        {
+            print_help (argv [0]);
+            exit (1);
+        }
+        else if (streq (argv[i], "-b")) {
+            str_p = &broker_endp;
+        }
+        /* Fallout for options with parameters */
+        else {
+            *str_p = strdup (argv[i]);
+        }
+    }
+
+    /* Set default broker address */
+    if (broker_endp == NULL) {
+        broker_endp = strdup ("ipc://"DFLT_BIND_FOLDER"/"DFLT_BIND_ADDR);
+    }
+
+    bpm_client_t *bpm_client = bpm_client_new (broker_endp, verbose);
+
     for (i = 0; i < 32768; ++i) {
         uint32_t leds = (1 << 1);
         unsigned int j;
@@ -65,5 +111,8 @@ int main (int argc, char *argv [])
     }
 
     bpm_client_destroy (&bpm_client);
+    str_p = &broker_endp;
+    free (*str_p);
+    broker_endp = NULL;
     return 0;
 }
