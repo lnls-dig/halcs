9ad24bd75cee35e7fdde14f7f8146f8b1af2b68c
Lucas Russo <lerwys@gmail.com>
2014-04-25 16:51:41 -0300

    hal/sm_io/*/fmc130m_4ch/fmc130m_4ch.mk: update Makefile
    Added the recently created objects to Makefile

--- hal/sm_io/modules/fmc130m_4ch/fmc130m_4ch.mk
+++ hal/sm_io/modules/fmc130m_4ch/fmc130m_4ch.mk
@@ -1,5 +1,6 @@
 sm_io_fmc130m_4ch_DIR = hal/sm_io/modules/fmc130m_4ch
 
-sm_io_fmc130m_4ch_OBJS = $(sm_io_fmc130m_4ch_DIR)/sm_io_fmc130m_4ch.o
+sm_io_fmc130m_4ch_OBJS = $(sm_io_fmc130m_4ch_DIR)/sm_io_fmc130m_4ch_core.o \
+				$(sm_io_fmc130m_4ch_DIR)/sm_io_fmc130m_4ch_exp.o
 
 sm_io_fmc130m_4ch_INCLUDE_DIRS = $(sm_io_fmc130m_4ch_DIR)
