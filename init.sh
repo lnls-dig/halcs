8b2e9e8643adbce586aec39d333a600f95587fe0
Lucas Russo <lerwys@gmail.com>
2014-05-15 19:18:52 -0300

    init.sh: remove restriction on the use of IPC
    Now, the init.sh scrip allows the usage of the
    TCP transport layer

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Broker Endpoint
-EXPECTED_ARGS=1
+EXPECTED_ARGS=2
 
 if [ "$(id -u)" != "0" ]
 then
@@ -11,19 +11,25 @@ fi
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-	echo "Error: Broker endpoint not set!"
-	echo "Usage: `basename $0` {broker endpoint}"
-	exit 1;
+	echo "Error: Wrong arguments!"
+	echo "Usage: `basename $0` {transport type = [ipc|tcp]} {broker endpoint}"
+	exit 1
 fi
 
-broker_endp=$1
+transport=$1
+broker_endp=$2
 
 # Launch Device Manager
-./dev_mngr -v -b "ipc://"$broker_endp &
+./dev_mngr -v -b ${transport}://${broker_endp} &
+# Wait until dev_mngr has registred the signal handlers
+sleep 1
 # Signal dev_mngr of a "new" PCIe device
 killall -SIGUSR1 dev_mngr
 
-# FIXME:Wait until worker has been created
+# Wait until worker has been created
 sleep 1
-# Change IPC permissions
-chmod 777 $broker_endp
+# Change IPC permissions if transport = ipc
+if [ "$transport" = "ipc" ]
+then
+    chmod 777 $broker_endp
+fi
