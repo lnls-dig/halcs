ecbcbb1c217d4dcebae224e44d092c36f5ef6b65
Juliano Murari <juliano.murari@gmail.com>
2014-05-23 15:49:57 -0300

    hal/sm_io/modules/modules.mk: add new SMIO module

--- hal/sm_io/modules/modules.mk
+++ hal/sm_io/modules/modules.mk
@@ -1,9 +1,12 @@
-include hal/sm_io/modules/fmc130m_4ch/fmc130m_4ch.mk
+include hal/sm_io/modules/fmc130m_4ch/fmc130m_4ch.mk \
+	hal/sm_io/modules/acq/acq.mk
 
 sm_io_modules_DIR = hal/sm_io/modules
 
 sm_io_modules_OBJS = $(sm_io_modules_DIR)/sm_io_mod_dispatch.o \
-		     $(sm_io_fmc130m_4ch_OBJS)
+		     $(sm_io_fmc130m_4ch_OBJS) \
+		     $(sm_io_acq_OBJS)
 
 sm_io_modules_INCLUDE_DIRS = $(sm_io_modules_DIR) \
-			     $(sm_io_fmc130m_4ch_DIR)
+			     $(sm_io_fmc130m_4ch_DIR)\
+			     $(sm_io_acq_DIR)
