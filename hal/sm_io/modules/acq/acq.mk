7782b85a87a662e9c9ea0c7eb8b03c2ab41269ee
Lucas Russo <lerwys@gmail.com>
2014-05-25 11:56:21 -0300

    hal/**/_codes.c: removed unused code structure
    These structures were not used anywhere in the
    code

--- hal/sm_io/modules/acq/acq.mk
+++ hal/sm_io/modules/acq/acq.mk
@@ -2,7 +2,6 @@ sm_io_acq_DIR = hal/sm_io/modules/acq
 
 sm_io_acq_OBJS = $(sm_io_acq_DIR)/sm_io_acq_core.o \
 		 $(sm_io_acq_DIR)/sm_io_acq_exp.o \
-		 $(sm_io_acq_DIR)/sm_io_acq_codes.o \
 		 $(sm_io_acq_DIR)/ddr3_map.o
 
 sm_io_acq_INCLUDE_DIRS = $(sm_io_acq_DIR)
