890b8275eb73b8efd834a224a4d0771b17eb2c59
Lucas Russo <lerwys@gmail.com>
2014-05-25 12:02:03 -0300

    libclient/bpm_client_codes.*: add include to general SMIO codes
    Now, we can simply reference the general SMIO file
    that includes all other SMIO codes.
    
    Also, removed unused code structure

--- libclient/bpm_client_codes.h
+++ libclient/bpm_client_codes.h
@@ -8,29 +8,6 @@
 #ifndef _BPM_CLIENT_CODES_H_
 #define _BPM_CLIENT_CODES_H_
 
-#include <inttypes.h>
-
-/* Messaging OPCODES */
-#define BPM_CLIENT_OPCODE_LEDS_SIZE        (sizeof (uint32_t))
-#define BPM_CLIENT_OPCODE_TYPE             uint32_t
-
-#define BPM_CLIENT_OPCODE_LEDS             0
-#define BPM_CLIENT_OPCODE_END              1
-
-typedef BPM_CLIENT_OPCODE_TYPE bpm_client_opcode_t;
-
-extern const bpm_client_opcode_t bpm_client_opcodes [BPM_CLIENT_OPCODE_END];
-
-/* Messaging Reply CODES */
-#define BPM_CLIENT_RECODE_SIZE              (sizeof(uint32_t))
-#define BPM_CLIENT_RECODE_TYPE              uint32_t
-
-#define BPM_CLIENT_RECODE_OK               0
-#define BPM_CLIENT_RECODE_ERR              1
-#define BPM_CLIENT_RECODE_END              2
-
-typedef BPM_CLIENT_RECODE_TYPE bpm_client_recode_t;
-
-extern const bpm_client_recode_t bpm_client_recodes [BPM_CLIENT_RECODE_END];
+#include "sm_io_codes.h"
 
 #endif
