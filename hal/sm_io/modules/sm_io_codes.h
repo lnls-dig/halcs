5af0f30c7efa216e5f41e31b2a64731872bd8b25
Lucas Russo <lerwys@gmail.com>
2014-05-25 11:57:14 -0300

    hal/sm_io/*/sm_io_codes.h: include all SMIO codes in a single file
    This will be useful for the libbpmclient library
    that needs to know the SMIO opcodes and error codes

---
+++ hal/sm_io/modules/sm_io_codes.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2014 LNLS (www.lnls.br)
+ * Author: Lucas Russo <lucas.russo@lnls.br>
+ *
+ * Released according to the GNU LGPL, version 3 or any later version.
+ */
+
+#ifndef _SM_IO_CODES_H_
+#define _SM_IO_CODES_H_
+
+/* Include all module's codes */
+#include "sm_io_fmc130m_4ch_codes.h"
+#include "sm_io_acq_codes.h"
+
+#endif
