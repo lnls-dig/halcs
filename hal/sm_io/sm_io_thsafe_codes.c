095a96113b2a967f691e6a97aa6b9aa805236ccf
Lucas Russo <lerwys@gmail.com>
2014-08-21 09:34:03 -0300

    hal/sm_io/*_thsafe_codes.*: add more semantic macro names

--- hal/sm_io/sm_io_thsafe_codes.c
+++ hal/sm_io/sm_io_thsafe_codes.c
@@ -8,19 +8,19 @@
 #include <sm_io_thsafe_codes.h>
 
 const uint32_t thsafe_opcodes [THSAFE_OPCODE_END] = {
-    [0]  = THSAFE_OPEN,
-    [1]  = THSAFE_RELEASE,
-    [2]  = THSAFE_READ_16,
-    [3]  = THSAFE_READ_32,
-    [4]  = THSAFE_READ_64,
-    [5]  = THSAFE_WRITE_16,
-    [6]  = THSAFE_WRITE_32,
-    [7]  = THSAFE_WRITE_64,
-    [8]  = THSAFE_READ_BLOCK,
-    [9]  = THSAFE_WRITE_BLOCK,
-    [10] = THSAFE_READ_DMA,
-    [11] = THSAFE_WRITE_DMA
-//    [12] = THSAFE_READ_INFO
+    [0]  = THSAFE_OPCODE_OPEN,
+    [1]  = THSAFE_OPCODE_RELEASE,
+    [2]  = THSAFE_OPCODE_READ_16,
+    [3]  = THSAFE_OPCODE_READ_32,
+    [4]  = THSAFE_OPCODE_READ_64,
+    [5]  = THSAFE_OPCODE_WRITE_16,
+    [6]  = THSAFE_OPCODE_WRITE_32,
+    [7]  = THSAFE_OPCODE_WRITE_64,
+    [8]  = THSAFE_OPCODE_READ_BLOCK,
+    [9]  = THSAFE_OPCODE_WRITE_BLOCK,
+    [10] = THSAFE_OPCODE_READ_DMA,
+    [11] = THSAFE_OPCODE_WRITE_DMA
+//    [12] = THSAFE_OPCODE_READ_INFO
 };
 
 const uint32_t thsafe_reply_opcodes [THSAFE_REPLY_END] = {
