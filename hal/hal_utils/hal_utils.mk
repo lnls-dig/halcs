12586801c7b7f0bedbc211a90f60299a1eaa6e98
Lucas Russo <lerwys@gmail.com>
2014-08-26 17:35:00 -0300

    hal/hal_utils/hal_utils.mk: update makefile

--- hal/hal_utils/hal_utils.mk
+++ hal/hal_utils/hal_utils.mk
@@ -1,8 +1,11 @@
 hal_utils_DIR = hal/hal_utils
 
+# We depend on "msg.c" fucntions to determine which message
+# we are dealing with
 hal_utils_OBJS = $(hal_utils_DIR)/hal_utils.o \
 		 $(hal_utils_DIR)/hal_utils_err.o \
 		 $(hal_utils_DIR)/dispatch_table.o \
+		 $(msg_DIR)/msg.o 
 
 hal_utils_INCLUDE_DIRS = $(hal_utils_DIR)
 
