4113d6d87e3f28a9cb98c4d5bc9018a17654df8f
Lucas Russo <lerwys@gmail.com>
2014-08-26 17:18:22 -0300

    hal/msg/exp_ops/exp_msg_zmq.c: remove unsued HAL macros

--- hal/msg/exp_ops/exp_msg_zmq.c
+++ hal/msg/exp_ops/exp_msg_zmq.c
@@ -6,33 +6,10 @@
  */
 
 #include "exp_msg_zmq.h"
-#include "hal_assert.h"
-
-/* Undef ASSERT_ALLOC to avoid conflicting with other ASSERT_ALLOC */
-#ifdef ASSERT_TEST
-#undef ASSERT_TEST
-#endif
-#define ASSERT_TEST(test_boolean, err_str, err_goto_label, /* err_core */ ...) \
-    ASSERT_HAL_TEST(test_boolean, MSG, "[exp_msg:zmq]",     \
-            err_str, err_goto_label, /* err_core */ __VA_ARGS__)
-
-#ifdef ASSERT_ALLOC
-#undef ASSERT_ALLOC
-#endif
-#define ASSERT_ALLOC(ptr, err_goto_label, /* err_core */ ...) \
-    ASSERT_HAL_ALLOC(ptr, MSG, "[exp_msg:zmq]",             \
-            msg_err_str(MSG_ERR_ALLOC),                     \
-            err_goto_label, /* err_core */ __VA_ARGS__)
-
-#ifdef CHECK_ERR
-#undef CHECK_ERR
-#endif
-#define CHECK_ERR(err, err_type)                            \
-    CHECK_HAL_ERR(err, MSG, "[exp_msg:zmq]",                \
-            msg_err_str (err_type))
 
 bool exp_msg_zmq_is (void *self)
 {
     assert (self);
     return ((exp_msg_zmq_t *) self)->tag == EXP_MSG_ZMQ_TAG;
 }
+
