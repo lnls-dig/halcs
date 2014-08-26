d6ba102ab9ab9e3cffce7398f41bb65e238dabc4
Lucas Russo <lerwys@gmail.com>
2014-08-26 17:42:13 -0300

    hal/msg/exp_ops/ops.mk: update makefile

--- hal/msg/exp_ops/ops.mk
+++ hal/msg/exp_ops/ops.mk
@@ -1,6 +1,5 @@
 exp_ops_DIR = hal/msg/exp_ops
 
-exp_ops_OBJS = $(exp_ops_DIR)/exp_msg_zmq.o \
-		$(exp_ops_DIR)/exp_ops.o
+exp_ops_OBJS = $(exp_ops_DIR)/exp_msg_zmq.o
 
 exp_ops_INCLUDE_DIRS = $(exp_ops_DIR)
