12205fc7e6864b60034d916a62ab30af67165bd8
Lucas Russo <lerwys@gmail.com>
2014-10-16 11:10:00 -0300

    hal/sm_io/*/acq/*: add better board differentiation
    Now, the acquisition channels are pulled from the include
    files and just "assembled" in the hal/sm_io/modules/acq
    folder.

--- hal/sm_io/modules/acq/ddr3_map.c
+++ hal/sm_io/modules/acq/ddr3_map.c
@@ -1,9 +1,122 @@
 #include "ddr3_map.h"
 
+#if defined(__BOARD_ML605__)
+
 const acq_buf_t __acq_buf[END_CHAN_ID] = {
-  {ADC_CHAN_ID, DDR3_ADC_START_ADDR, DDR3_ADC_END_ADDR, DDR3_ADC_MAX_SAMPLES, DDR3_ADC_SAMPLE_SIZE},
-  {TBTAMP_CHAN_ID, DDR3_TBTAMP_START_ADDR, DDR3_TBTAMP_END_ADDR, DDR3_TBTAMP_MAX_SAMPLES, DDR3_TBTAMP_SAMPLE_SIZE},
-  {TBTPOS_CHAN_ID, DDR3_TBTPOS_START_ADDR, DDR3_TBTPOS_END_ADDR, DDR3_TBTPOS_MAX_SAMPLES, DDR3_TBTPOS_SAMPLE_SIZE},
-  {FOFBAMP_CHAN_ID, DDR3_FOFBAMP_START_ADDR, DDR3_FOFBAMP_END_ADDR, DDR3_FOFBAMP_MAX_SAMPLES, DDR3_FOFBAMP_SAMPLE_SIZE},
-  {FOFBPOS_CHAN_ID, DDR3_FOFBPOS_START_ADDR, DDR3_FOFBPOS_END_ADDR, DDR3_FOFBPOS_MAX_SAMPLES, DDR3_FOFBPOS_SAMPLE_SIZE}
+  {
+      .id = ADC0_CHAN_ID,
+      .start_addr = DDR3_ADC0_START_ADDR,
+      .end_addr = DDR3_ADC0_END_ADDR,
+      .max_samples = DDR3_ADC0_MAX_SAMPLES,
+      .sample_size = DDR3_ADC_SAMPLE_SIZE
+  },
+  {
+      .id = TBTAMP0_CHAN_ID,
+      .start_addr = DDR3_TBTAMP0_START_ADDR,
+      .end_addr = DDR3_TBTAMP0_END_ADDR,
+      .max_samples =  DDR3_TBTAMP0_MAX_SAMPLES,
+      .sample_size = DDR3_TBTAMP_SAMPLE_SIZE
+  },
+  {
+      .id = TBTPOS0_CHAN_ID,
+      .start_addr = DDR3_TBTPOS0_START_ADDR,
+      .end_addr = DDR3_TBTPOS0_END_ADDR,
+      .max_samples =DDR3_TBTPOS0_MAX_SAMPLES,
+      .sample_size = DDR3_TBTPOS_SAMPLE_SIZE
+  },
+  {
+      .id = FOFBAMP0_CHAN_ID,
+      .start_addr = DDR3_FOFBAMP0_START_ADDR,
+      .end_addr = DDR3_FOFBAMP0_END_ADDR,
+      .max_samples =  DDR3_FOFBAMP0_MAX_SAMPLES,
+      .sample_size = DDR3_FOFBAMP_SAMPLE_SIZE
+  },
+  {
+      .id = FOFBPOS0_CHAN_ID,
+      .start_addr = DDR3_FOFBPOS0_START_ADDR,
+      .end_addr = DDR3_FOFBPOS0_END_ADDR,
+      .max_samples = DDR3_FOFBPOS0_MAX_SAMPLES,
+      .sample_size = DDR3_FOFBPOS_SAMPLE_SIZE
+  }
 };
+
+#elif defined(__BOARD_AFCV3__)
+
+const acq_buf_t __acq_buf[END_CHAN_ID] = {
+  {
+      .id = ADC0_CHAN_ID,
+      .start_addr = DDR3_ADC0_START_ADDR,
+      .end_addr = DDR3_ADC0_END_ADDR,
+      .max_samples = DDR3_ADC0_MAX_SAMPLES,
+      .sample_size = DDR3_ADC0_SAMPLE_SIZE
+  },
+  {
+      .id = TBTAMP0_CHAN_ID,
+      .start_addr = DDR3_TBTAMP0_START_ADDR,
+      .end_addr = DDR3_TBTAMP0_END_ADDR,
+      .max_samples = DDR3_TBTAMP0_MAX_SAMPLES,
+      .sample_size = DDR3_TBTAMP0_SAMPLE_SIZE
+  },
+  {
+      .id = TBTPOS0_CHAN_ID,
+      .start_addr = DDR3_TBTPOS0_START_ADDR,
+      .end_addr = DDR3_TBTPOS0_END_ADDR,
+      .max_samples = DDR3_TBTPOS0_MAX_SAMPLES,
+      .sample_size = DDR3_TBTPOS0_SAMPLE_SIZE
+  },
+  {
+      .id = FOFBAMP0_CHAN_ID,
+      .start_addr = DDR3_FOFBAMP0_START_ADDR,
+      .end_addr = DDR3_FOFBAMP0_END_ADDR,
+      .max_samples = DDR3_FOFBAMP0_MAX_SAMPLES,
+      .sample_size = DDR3_FOFBAMP0_SAMPLE_SIZE
+  },
+  {
+      .id = FOFBPOS0_CHAN_ID,
+      .start_addr = DDR3_FOFBPOS0_START_ADDR,
+      .end_addr = DDR3_FOFBPOS0_END_ADDR,
+      .max_samples = DDR3_FOFBPOS0_MAX_SAMPLES,
+      .sample_size = DDR3_FOFBPOS0_SAMPLE_SIZE
+  },
+  {
+      .id = ADC1_CHAN_ID,
+      .start_addr = DDR3_ADC1_START_ADDR,
+      .end_addr = DDR3_ADC1_END_ADDR,
+      .max_samples = DDR3_ADC1_MAX_SAMPLES,
+      .sample_size = DDR3_ADC1_SAMPLE_SIZE
+  },
+  {
+      .id = TBTAMP1_CHAN_ID,
+      .start_addr = DDR3_TBTAMP1_START_ADDR,
+      .end_addr = DDR3_TBTAMP1_END_ADDR,
+      .max_samples = DDR3_TBTAMP1_MAX_SAMPLES,
+      .sample_size = DDR3_TBTAMP1_SAMPLE_SIZE
+  },
+  {
+      .id = TBTPOS1_CHAN_ID,
+      .start_addr = DDR3_TBTPOS1_START_ADDR,
+      .end_addr = DDR3_TBTPOS1_END_ADDR,
+      .max_samples = DDR3_TBTPOS1_MAX_SAMPLES,
+      .sample_size = DDR3_TBTPOS1_SAMPLE_SIZE
+  },
+  {
+      .id = FOFBAMP1_CHAN_ID,
+      .start_addr = DDR3_FOFBAMP1_START_ADDR,
+      .end_addr = DDR3_FOFBAMP1_END_ADDR,
+      .max_samples = DDR3_FOFBAMP1_MAX_SAMPLES,
+      .sample_size = DDR3_FOFBAMP1_SAMPLE_SIZE
+  },
+  {
+      .id = FOFBPOS1_CHAN_ID,
+      .start_addr = DDR3_FOFBPOS1_START_ADDR,
+      .end_addr = DDR3_FOFBPOS1_END_ADDR,
+      .max_samples = DDR3_FOFBPOS1_MAX_SAMPLES,
+      .sample_size = DDR3_FOFBPOS1_SAMPLE_SIZE
+  }
+};
+
+#else
+
+#error "Could not initialize acq_buf. Unsupported board!"
+
+#endif
