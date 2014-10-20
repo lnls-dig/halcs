ce109b390f826ed7e840d4d4e757a4f6db17e9d5
Lucas Russo <lerwys@gmail.com>
2014-10-20 15:19:10 -0200

    examples/plot_data.sh: add simple repetitive data plot

--- examples/plot_data.sh
+++ examples/plot_data.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-	echo "Usage: `basename $0` {broker endpoint} {number of samples} {channel number}"
+	echo "Usage: `basename $0` {broker endpoint} {number of samples} {channel number} {bpm number}"
 	exit 1;
 fi
 
 broker_endp=$1
 nsamples=$2
 ch_number=$3
+bpm_number=$4
 
 DATAPLOT_FILE=".feedgnuplot_data"
 
 acquire_samples () {
-    if [ $# -ne 3 ]
+    if [ $# -ne 4 ]
     then
-        echo "acquire_samples usage: {broker endpoint} {number of samples} {channel number}"
+        echo "acquire_samples usage: {broker endpoint} {number of samples} {channel number} {bpm number}"
         exit 1;
     fi
 
     while :
     do
-        ./acq -b $1 -s $2 -ch $3 > $DATAPLOT_FILE
-        sleep 1
+        ./acq -b $1 -s $2 -ch $3 -bpm $4 > ${DATAPLOT_FILE}.$4
+        sleep 2
     done
 }
 
 plot_data () {
-    tail -n $1 -f $DATAPLOT_FILE |
+    tail -n $1 -f ${DATAPLOT_FILE}.$2 |
     feedgnuplot --lines \
         --stream \
         --domain \
@@ -42,7 +43,7 @@ plot_data () {
         --legend 3 'D'
 }
 
-acquire_samples $broker_endp $nsamples $ch_number &
+acquire_samples $broker_endp $nsamples $ch_number $bpm_number &
 sleep 2
-plot_data $nsamples
+plot_data $nsamples $bpm_number
 
