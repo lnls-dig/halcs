6567fc6ef52ad777e79aa2f2a6face48f9cbe690
Lucas Russo <lerwys@gmail.com>
2014-10-28 16:32:18 -0200

    hal/sm_io/*/acq/ddr3_map.c: fix ML605 DDR3 address typos

--- hal/sm_io/modules/acq/ddr3_map.c
+++ hal/sm_io/modules/acq/ddr3_map.c
@@ -9,35 +9,35 @@ const acq_buf_t __acq_buf[END_CHAN_ID] = {
         .start_addr = DDR3_ADC0_START_ADDR,
         .end_addr = DDR3_ADC0_END_ADDR,
         .max_samples = DDR3_ADC0_MAX_SAMPLES,
-        .sample_size = DDR3_ADC_SAMPLE_SIZE
+        .sample_size = DDR3_ADC0_SAMPLE_SIZE
     },
     {
         .id = TBTAMP0_CHAN_ID,
         .start_addr = DDR3_TBTAMP0_START_ADDR,
         .end_addr = DDR3_TBTAMP0_END_ADDR,
         .max_samples =  DDR3_TBTAMP0_MAX_SAMPLES,
-        .sample_size = DDR3_TBTAMP_SAMPLE_SIZE
+        .sample_size = DDR3_TBTAMP0_SAMPLE_SIZE
     },
     {
         .id = TBTPOS0_CHAN_ID,
         .start_addr = DDR3_TBTPOS0_START_ADDR,
         .end_addr = DDR3_TBTPOS0_END_ADDR,
         .max_samples =DDR3_TBTPOS0_MAX_SAMPLES,
-        .sample_size = DDR3_TBTPOS_SAMPLE_SIZE
+        .sample_size = DDR3_TBTPOS0_SAMPLE_SIZE
     },
     {
         .id = FOFBAMP0_CHAN_ID,
         .start_addr = DDR3_FOFBAMP0_START_ADDR,
         .end_addr = DDR3_FOFBAMP0_END_ADDR,
         .max_samples =  DDR3_FOFBAMP0_MAX_SAMPLES,
-        .sample_size = DDR3_FOFBAMP_SAMPLE_SIZE
+        .sample_size = DDR3_FOFBAMP0_SAMPLE_SIZE
     },
     {
         .id = FOFBPOS0_CHAN_ID,
         .start_addr = DDR3_FOFBPOS0_START_ADDR,
         .end_addr = DDR3_FOFBPOS0_END_ADDR,
         .max_samples = DDR3_FOFBPOS0_MAX_SAMPLES,
-        .sample_size = DDR3_FOFBPOS_SAMPLE_SIZE
+        .sample_size = DDR3_FOFBPOS0_SAMPLE_SIZE
     },
     {
         .id = MONITAMP0_CHAN_ID,
