62bc89b9b9c69deab7e86393fb7dc425bbf9291e
Lucas Russo <lerwys@gmail.com>
2014-11-05 13:53:31 -0200

    hal/include/board/afcv3/*: remove channel #2 definitions
    The new FPGA firmware for AFCv3 has 2 acquisition cores
    with 9 channels. Thus, we don't need the description of
    both channels.

--- hal/include/board/afcv3/acq_chan_afcv3.h
+++ hal/include/board/afcv3/acq_chan_afcv3.h
@@ -3,6 +3,8 @@
 
 #include <acq_chan_gen_defs.h>
 
+#define NUM_ACQ_CORE_SMIOS              2
+
 /************************ Acquistion 0 Channel Parameters **************/
 
 /* ADC */
@@ -31,7 +33,7 @@
 
 /* MONIT AMP */
 #define MONITAMP0_CHAN_ID               (FOFBPOS0_CHAN_ID + 1)
-#define MONITAMP0_SAMPLE_SIZE           16 /* 16 Bytes -> FOFBAMP0 = 32-bit / FOFBAMP1 = 32-bit ... */
+#define MONITAMP0_SAMPLE_SIZE           16 /* 16 Bytes -> MONITAMP0 = 32-bit / MONITAMP1 = 32-bit ... */
 
 /* MONIT POS */
 #define MONITPOS0_CHAN_ID               (MONITAMP0_CHAN_ID + 1)
@@ -41,46 +43,8 @@
 #define MONIT1POS0_CHAN_ID              (MONITPOS0_CHAN_ID + 1)
 #define MONIT1POS0_SAMPLE_SIZE          16 /* 16 Bytes -> X = 32-bit / Y = 32-bit ... */
 
-/************************ Acquistion 1 Channel Parameters **************/
-
-/* ADC */
-#define ADC1_CHAN_ID                    (MONIT1POS0_CHAN_ID + 1)
-#define ADC1_SAMPLE_SIZE                8 /* 8 Bytes -> ADC0 = 16-bit / ADC1 = 16-bit ... */
-
-/* MIXER I */
-#define MIX1_CHAN_ID                    (ADC1_CHAN_ID + 1)
-#define MIX1_SAMPLE_SIZE                16 /* 16 Bytes -> MIX0 = 32-bit / MIX1 = 32-bit ... */
-
-/* TBT AMP */
-#define TBTAMP1_CHAN_ID                 (MIX1_CHAN_ID + 1)
-#define TBTAMP1_SAMPLE_SIZE             16 /* 16 Bytes -> TBTAMP1 = 32-bit / TBTAMP1 = 32-bit ... */
-
-/* TBT POS */
-#define TBTPOS1_CHAN_ID                 (TBTAMP1_CHAN_ID + 1)
-#define TBTPOS1_SAMPLE_SIZE             16 /* 16 Bytes -> X = 32-bit / Y = 32-bit ... */
-
-/* FOFB AMP */
-#define FOFBAMP1_CHAN_ID                (TBTPOS1_CHAN_ID + 1)
-#define FOFBAMP1_SAMPLE_SIZE            16 /* 16 Bytes -> FOFBAMP1 = 32-bit / FOFBAMP1 = 32-bit ... */
-
-/* FOFB POS */
-#define FOFBPOS1_CHAN_ID                (FOFBAMP1_CHAN_ID + 1)
-#define FOFBPOS1_SAMPLE_SIZE            16  /* 16 Bytes -> X = 32-bit / Y = 32-bit ... */
-
-/* MONIT AMP */
-#define MONITAMP1_CHAN_ID               (FOFBPOS1_CHAN_ID + 1)
-#define MONITAMP1_SAMPLE_SIZE           16 /* 16 Bytes -> FOFBAMP1 = 32-bit / FOFBAMP1 = 32-bit ... */
-
-/* MONIT POS */
-#define MONITPOS1_CHAN_ID               (MONITAMP1_CHAN_ID + 1)
-#define MONITPOS1_SAMPLE_SIZE           16 /* 16 Bytes -> X = 32-bit / Y = 32-bit ... */
-
-/* MONIT1 POS */
-#define MONIT1POS1_CHAN_ID              (MONITPOS1_CHAN_ID + 1)
-#define MONIT1POS1_SAMPLE_SIZE          16 /* 16 Bytes -> X = 32-bit / Y = 32-bit ... */
-
 /* End of channels placeholder */
-#define END_CHAN_ID                     (MONIT1POS1_CHAN_ID + 1)
+#define END_CHAN_ID                     (MONIT1POS0_CHAN_ID + 1)
 
 #endif
 
