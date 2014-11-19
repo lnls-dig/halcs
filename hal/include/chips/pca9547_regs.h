d5a403a4dbac6e494332281c0b5d053630e5479c
Lucas Russo <lerwys@gmail.com>
2014-11-19 18:27:40 -0200

    hal/sm_io/chips/*: add new I2C PCA9547 switch drivers

---
+++ hal/include/chips/pca9547_regs.h
@@ -0,0 +1,31 @@
+/*
+* Copyright (C) 2014 LNLS (www.lnls.br)
+* Author: Lucas Russo <lucas.russo@lnls.br>
+*
+* Released according to the GNU LGPL, version 3 or any later version.
+*/
+
+#ifndef _PCA9547_REGS_H_
+#define _PCA9547_REGS_H_
+
+/* Copied from wb_acq_core_regs.h */
+#if defined( __GNUC__)
+#define PACKED __attribute__ ((packed))
+#else
+#error "Unsupported compiler?"
+#endif
+
+/* Copied from wb_acq_core_regs.h */
+#ifndef __WBGEN2_MACROS_DEFINED__
+#define __WBGEN2_MACROS_DEFINED__
+#define WBGEN2_GEN_MASK(offset, size) (((1<<(size))-1) << (offset))
+#define WBGEN2_GEN_WRITE(value, offset, size) (((value) & ((1<<(size))-1)) << (offset))
+#define WBGEN2_GEN_READ(reg, offset, size) (((reg) >> (offset)) & ((1<<(size))-1))
+#define WBGEN2_SIGN_EXTEND(value, bits) (((value) & (1<<bits) ? ~((1<<(bits))-1): 0 ) | (value))
+#endif
+
+#define PCA9547_DATA_TRANS_SIZE                             8                       /* in bits */
+#define PCA9547_BYTE_TRANS_SIZE                             PCA9547_DATA_TRANS_SIZE /* in bits */
+
+#endif
+
---
+++ hal/sm_io/chips/sm_ch_pca9547.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 LNLS (www.lnls.br)
+ * Author: Lucas Russo <lucas.russo@lnls.br>
+ *
+ * Released according to the GNU LGPL, version 3 or any later version.
+ */
+
+#ifndef _SM_CH_PCA9547_H_
+#define _SM_CH_PCA9547_H_
+
+#include <inttypes.h>
+#include <sys/types.h>
+
+#include "sm_pr_i2c.h"
+#include "sm_ch_err.h"
+
+struct _smch_pca9547_t {
+    smpr_t *i2c;                    /* I2C protocol object */
+    uint32_t addr;                  /* I2C address for this PCA9547 chip */
+};
+
+/* Opaque sm_ch_pca9547 structure */
+typedef struct _smch_pca9547_t smch_pca9547_t;
+
+/***************** Our methods *****************/
+
+/* Creates a new instance of the SMCH PCA9547 */
+smch_pca9547_t * smch_pca9547_new (smio_t *parent, uint32_t base, uint32_t addr,
+        int verbose);
+/* Destroy an instance of the SMCH PCA9547 */
+smch_err_e smch_pca9547_destroy (smch_pca9547_t **self_p);
+
+/* Read/Write to PCA9547 single bytes */
+smch_err_e smch_pca9547_write_8 (smch_pca9547_t *self, const uint8_t *data);
+smch_err_e smch_pca9547_read_8 (smch_pca9547_t *self, uint8_t *data);
+
+#endif
