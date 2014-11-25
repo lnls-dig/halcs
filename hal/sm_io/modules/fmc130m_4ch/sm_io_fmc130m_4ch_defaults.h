38a980fb017bf8ec2990527a75f29d2123b6f060
Lucas Russo <lerwys@gmail.com>
2014-11-25 14:18:02 -0200

    hal/sm_io/*/fmc130m_4ch/*_defaults.h: add Si57x defaults

--- hal/sm_io/modules/fmc130m_4ch/sm_io_fmc130m_4ch_defaults.h
+++ hal/sm_io/modules/fmc130m_4ch/sm_io_fmc130m_4ch_defaults.h
@@ -9,10 +9,14 @@
 #define _FMC130M_4CH_DEFAULTS_H_
 
 #include "sm_io_err.h"
+#include "sm_ch_si57x.h"
+#include "sm_ch_pca9547.h"
 
-#define FMC130M_4CH_DFLT_PLL_FUNC           0x1
-#define FMC130M_4CH_DFLT_PCA9547_CFG        0x4         /* Connect I2C switch master to output 4 */
-#define FMC130M_4CH_DFLT_SI571_OE           0x1
+#define FMC130M_4CH_DFLT_PLL_FUNC                   0x1
+#define FMC130M_4CH_DFLT_PCA9547_CFG                SMCH_PCA9547_NO_CHANNEL         /* No channel selected */
+#define FMC130M_4CH_DFLT_SI571_OE                   0x1
+
+#define FMC130M_4CH_DFLT_SI57X_FOUT_FACTORY         SMCH_SI57X_FOUT_FACTORY_DFLT
 
 smio_err_e fmc130m_4ch_config_defaults (char *broker_endp, char *service,
         const char *log_file_name);
