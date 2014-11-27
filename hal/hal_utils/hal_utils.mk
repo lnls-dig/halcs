0e317ab88d4938e02fd543a10bb0f86f1d2af539
Lucas Russo <lerwys@gmail.com>
2014-11-27 09:19:40 -0200

    hal/hal_utils/hal_math.*: add new math functions

--- hal/hal_utils/hal_utils.mk
+++ hal/hal_utils/hal_utils.mk
@@ -3,9 +3,10 @@ hal_utils_DIR = hal/hal_utils
 # We depend on "msg.c" fucntions to determine which message
 # we are dealing with
 hal_utils_OBJS = $(hal_utils_DIR)/hal_utils.o \
+		 $(hal_utils_DIR)/hal_math.o \
 		 $(hal_utils_DIR)/hal_utils_err.o \
 		 $(hal_utils_DIR)/dispatch_table.o \
-		 $(msg_DIR)/msg.o 
+		 $(msg_DIR)/msg.o
 
 hal_utils_INCLUDE_DIRS = $(hal_utils_DIR)
 
