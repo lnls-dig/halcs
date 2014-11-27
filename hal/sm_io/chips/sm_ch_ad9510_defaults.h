630943529a59964330030fe999152abdae7da0b1
Lucas Russo <lerwys@gmail.com>
2014-11-27 19:21:47 -0200

    hal/sm_io/chips/sm_ch_ad9510_defaults.h: add default configurations

---
+++ hal/sm_io/chips/sm_ch_ad9510_defaults.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 LNLS (www.lnls.br)
+ * Author: Lucas Russo <lucas.russo@lnls.br>
+ *
+ * Released according to the GNU LGPL, version 3 or any later version.
+ */
+
+#ifndef _SMCH_AD9510_DEFAULTS_H_
+#define _SMCH_AD9510_DEFAULTS_H_
+
+#include "ad9510_regs.h"
+
+#define SMCH_AD9510_DFLT_PLL_B_COUNTER                   10      /* Divide VXCO frequency by 10 */
+#define SMCH_AD9510_DFLT_PLL_R_COUNTER                   1       /* Divide Reference frequency by 1 */
+
+#endif
+
