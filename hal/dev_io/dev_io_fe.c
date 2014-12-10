fbfd3d07a7082849aad91db43e52f4a802555db6
Lucas Russo <lerwys@gmail.com>
2014-12-10 14:25:40 -0200

    hal/dev_io/dev_io_fe.*: add first stub for RFFE DEVIO (wip)
    This is a first stub for the RFFE DEVIO and will not work!

---
+++ hal/dev_io/dev_io_fe.c
@@ -0,0 +1,235 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>          /* getpid, getppid */
+#include <unistd.h>
+
+#include "czmq.h"
+#include "dev_io_fe.h"
+#include "debug_print.h"
+#include "ll_io_utils.h"
+#include "board.h"
+
+#define DEVIO_SERVICE_LEN       50
+
+static devio_err_e spwan_platform_smios (devio_t *devio);
+
+void print_help (char *program_name)
+{
+    printf( "RFFE BPM Device I/O\n"
+            "Usage: %s [options]\n"
+            "\t-h This help message\n"
+            "\t-d Daemon mode.\n"
+            "\t-v Verbose output\n"
+            "\t-t <device_type> Device type\n"
+            "\t-e <dev_entry> Device /dev entry\n"
+            "\t-i <dev_id> Device ID\n"
+            "\t-l <log_filename> Log filename\n"
+            "\t-b <broker_endpoint> Broker endpoint\n", program_name);
+}
+
+int main (int argc, char *argv[])
+{
+    int verbose = 0;
+    int daemonize = 0;
+    char *dev_type = NULL;
+    char *dev_entry = NULL;
+    char *dev_id_str = NULL;
+    uint32_t dev_id = 0;
+    char *broker_endp = NULL;
+    char *log_file_name = NULL;
+    char **str_p = NULL;
+    int i;
+
+    if (argc < 5) {
+        print_help (argv[0]);
+        exit (1);
+    }
+
+    /* FIXME: This is rather buggy! */
+    /* Simple handling of command-line options. This should be done
+     * with getopt, for instance*/
+    for (i = 1; i < argc; i++)
+    {
+        if (streq (argv[i], "-v")) {
+            verbose = 1;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Verbose mode set\n");
+        }
+        else if (streq (argv[i], "-d")) {
+            daemonize = 1;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Demonize mode set\n");
+        }
+        else if (streq (argv[i], "-t")) {
+            str_p = &dev_type;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Will set dev_type parameter\n");
+        }
+        else if (streq (argv[i], "-e")) {
+            str_p = &dev_entry;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Will set dev_entry parameter\n");
+        }
+        else if (streq (argv[i], "-i")) {
+            str_p = &dev_id_str;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Will set dev_id_str parameter\n");
+        }
+        else if (streq (argv[i], "-b")) {
+            str_p = &broker_endp;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Will set broker_endp parameter\n");
+        }
+        else if (streq (argv[i], "-l")) {
+            str_p = &log_file_name;
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Will set log filename\n");
+        }
+        else if (streq (argv[i], "-h")) {
+            print_help (argv[0]);
+            exit (1);
+        }
+        /* Fallout for options with parameters */
+        else {
+            if (str_p) {
+                *str_p = strdup (argv[i]);
+                DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Parameter set to \"%s\"\n", *str_p);
+            }
+        }
+    }
+
+    /* Daemonize dev_io */
+    if (daemonize != 0) {
+        int rc = daemon(0, 0);
+
+        if (rc != 0) {
+            perror ("[dev_io_fe] daemon");
+            goto err_exit;
+        }
+    }
+
+    llio_type_e llio_type = llio_str_to_type (dev_type);
+    /* Parse command-line options */
+    if (llio_type == INVALID_DEV) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_FATAL, "[dev_io_fe] Dev_type parameter is invalid\n");
+        goto err_exit;
+    }
+
+    /* Check for device entry */
+    if (dev_entry == NULL) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_INFO, "[dev_io_fe] Dev_entry parameter was not set. Exiting ...\n");
+        goto err_exit;
+    }
+
+    /* Check for device ID */
+    if (dev_id_str == NULL) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_INFO, "[dev_io_fe] Dev_id parameter was not set.\n"
+                "\tDefaulting it to the /dev file number ...\n");
+
+        int matches = sscanf (dev_entry, "/dev/fpga%u", &dev_id);
+        if (matches == 0) {
+            DBE_DEBUG (DBG_DEV_IO | DBG_LVL_FATAL, "[dev_io_fe] Dev_entry parameter is invalid.\n"
+                    "\tIt must be in the format \"/dev/fpga<device_number>\". Exiting ...\n");
+            goto err_exit;
+        }
+    }
+    /* Use the passed ID */
+    else {
+        dev_id = strtoul (dev_id_str, NULL, 10);
+    }
+
+    DBE_DEBUG (DBG_DEV_IO | DBG_LVL_INFO, "[dev_io_fe] Dev_id parameter was set to %u.\n",
+            dev_id);
+
+    /* We don't need it anymore */
+    str_p = &dev_type;
+    free (*str_p);
+    dev_type = NULL;
+    str_p = &dev_id_str;
+    free (*str_p);
+    dev_id_str = NULL;
+
+    /* Initilialize dev_io */
+    DBE_DEBUG (DBG_DEV_IO | DBG_LVL_TRACE, "[dev_io_fe] Creating DEVIO instance ...\n");
+
+    char devio_service_str [DEVIO_SERVICE_LEN];
+    snprintf (devio_service_str, DEVIO_SERVICE_LEN-1, "BPM%u:DEVIO", dev_id);
+    devio_service_str [DEVIO_SERVICE_LEN-1] = '\0'; /* Just in case ... */
+    devio_t *devio = devio_new (devio_service_str, dev_entry, llio_type,
+            broker_endp, verbose, log_file_name);
+    /* devio_t *devio = devio_new ("BPM0:DEVIO", *str_p, llio_type,
+            "tcp://localhost:5555", verbose); */
+
+    if (devio == NULL) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_FATAL, "[dev_io_fe] devio_new error!\n");
+        goto err_exit;
+    }
+
+    /* We don't need it anymore */
+    str_p = &dev_entry;
+    free (*str_p);
+    dev_entry = NULL;
+    str_p = &broker_endp;
+    free (*str_p);
+    broker_endp = NULL;
+
+    devio_err_e err = spwan_platform_smios (devio);
+    if (err != DEVIO_SUCCESS) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_FATAL, "[dev_io_fe] spwan_platform_smios error!\n");
+        goto err_devio;
+    }
+
+    /* err = devio_init_poller_sm (devio); */
+    err = devio_init_poller2_sm (devio);
+    if (err != DEVIO_SUCCESS) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_FATAL, "[dev_io_fe] devio_init_poller_sm error!\n");
+        goto err_devio;
+    }
+
+    while (!zctx_interrupted) {
+        /* Step 1: Loop though all the SDB records and intialize (boot) the
+         * smio modules*/
+        /* Step 2: Optionally, register the necessary smio modules specifying
+         * its ID and calling devio_register_sm */
+        /* Step 3: Poll all PIPE's sockets to determine if we have something to
+         * handle, like messages from smios */
+        /*      Step 3.5: If we do, call devio_handle_smio () and treat its
+         *      request as appropriate */
+
+        /* devio_poll_all_sm (devio); */
+        devio_poll2_all_sm (devio);
+    }
+
+err_devio:
+    devio_destroy (&devio);
+err_exit:
+    str_p = &log_file_name;
+    free (*str_p);
+    str_p = &broker_endp;
+    free (*str_p);
+    str_p = &dev_id_str;
+    free (*str_p);
+    str_p = &dev_entry;
+    free (*str_p);
+    str_p = &dev_type;
+    free (*str_p);
+    return 0;
+}
+
+static devio_err_e spwan_platform_smios (devio_t *devio)
+{
+    assert (devio);
+
+    uint32_t rffe_v2_id = 0x7af21909;
+    devio_err_e err = DEVIO_SUCCESS;
+
+    /* RFFE V2 only */
+/* #if defined (__AFE_RFFE_V1__) */
+#if defined (__AFE_RFFE_V2__)
+    DBE_DEBUG (DBG_DEV_IO | DBG_LVL_INFO, "[dev_io_fe] Spawning default SMIOs ...\n");
+    err = devio_register_sm (devio, rffe_v2_id, 0, 0);
+    if (err != DEVIO_SUCCESS) {
+        DBE_DEBUG (DBG_DEV_IO | DBG_LVL_FATAL, "[dev_io_fe] devio_register_sm error!\n");
+        goto err_register_sm;
+    }
+#else
+#error "AFE RFFE Board not supported!"
+#endif
+
+err_register_sm:
+    return err;
+}
+
---
+++ hal/dev_io/dev_io_fe.h
@@ -0,0 +1,6 @@
+#ifndef _DEV_BE_IO_
+#define _DEV_BE_IO_
+
+#include "dev_io_core.h"
+
+#endif
