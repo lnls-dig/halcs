4721a1a1b4bdfe06fb8ee0bf8651db54f2cd96f9
Lucas Russo <lerwys@gmail.com>
2015-01-12 11:21:46 -0200

    hal/sm_io/modules/rffe/*: add new RFFE exported module

---
+++ hal/sm_io/modules/rffe/sm_io_rffe_exp.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2014 LNLS (www.lnls.br)
+ * Author: Lucas Russo <lucas.russo@lnls.br>
+ *
+ * Released according to the GNU LGPL, version 3 or any later version.
+ */
+
+#ifndef _RFFE_H_
+#define _RFFE_H_
+
+#include "sm_io_bootstrap.h"
+#include "smio_thsafe_zmq_client.h"
+#include "exp_ops_codes.h"
+#include "sm_io_rffe_core.h"
+
+/* Known modules IDs */
+#define RFFE_DEVID       0x7af21909
+#define RFFE_NAME        "RFFE"
+
+extern const smio_bootstrap_ops_t rffe_bootstrap_ops;
+
+#endif
+
