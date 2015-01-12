082ca161f8451cd97780ecfecc768d17111958e3
Lucas Russo <lerwys@gmail.com>
2015-01-12 11:08:33 -0200

    hal/ll_io/ll_io_utils.*: add ethernet devices and generic type functions
    Now, we can reuse these functions for similar string to enum
    conversions

--- hal/ll_io/ll_io_utils.h
+++ hal/ll_io/ll_io_utils.h
@@ -8,6 +8,11 @@
 #ifndef _LL_IO_UTILS_H_
 #define _LL_IO_UTILS_H_
 
+#include <inttypes.h>
+
+#define LLIO_TYPE_END               0xFFFF
+#define LLIO_TYPE_NAME_END          ""
+
 /* Device type */
 enum _llio_type_e {
     GENERIC_DEV = 0,
@@ -19,15 +24,11 @@ enum _llio_type_e {
 typedef enum _llio_type_e llio_type_e;
 
 struct _llio_types_t {
-    llio_type_e type;
+    int type;
     char *name;
 };
 
 typedef struct _llio_types_t llio_types_t;
-
-/* FIXME: This is a hardcoded value that refers to an enum
-   that can grow arbitrarily */
-#define LLIO_TYPE_NUM               4
 
 #define GENERIC_DEV_STR             "generic"
 #define PCIE_DEV_STR                "pcie"
@@ -35,6 +36,15 @@ typedef struct _llio_types_t llio_types_t;
 #define INVALID_DEV_STR             "invalid"
 
 /************** Utility functions ****************/
+
+/* Generic mapping functions */
+
+/* Converts the LLIO type ID into a string */
+char *llio_gen_type_to_str (int type, const llio_types_t *llio_types);
+/* Converts a string to an LLIO type ID. If no match if found,
+ * a INVALID_DEV is returned */
+int llio_str_to_gen_type (const char *type_str,
+        const llio_types_t *llio_types);
 
 /* Converts the llio_type enumeration into a string */
 char *llio_type_to_str (llio_type_e type);
