d9084f3c7ac6bb2405f9293782277b3b994e536a
Lucas Russo <lerwys@gmail.com>
2015-01-12 11:32:21 -0200

    hal/dev_mngr/dev_mngr_dev_info.*: add info about devio
    As we merged the dev_io main program back into a single
    code, we need a way to pass additional arguments to it.
    So, we declare an dev_io_type parameter, meaning what SMIOs are
    spawned, and a smio_inst_id parameter, used for DEVIOs with a
    single SMIO, but multiple instances of it.

--- hal/dev_mngr/dev_mngr_dev_info.h
+++ hal/dev_mngr/dev_mngr_dev_info.h
@@ -10,7 +10,8 @@
 
 #include "czmq.h"
 #include "dev_mngr_err.h"
-#include "ll_io_utils.h"          /* LLIO types */
+#include "ll_io_utils.h"            /* LLIO types */
+#include "dev_io_utils.h"           /* DEVIO types */
 
 enum _devio_state_e {
     INACTIVE = 0,                       /* Nothing found yet */
@@ -23,6 +24,9 @@ enum _devio_state_e {
 struct _devio_info_t {
     uint32_t id;                        /* Device IO ID */
     llio_type_e type;                   /* Device type */
+    devio_type_e devio_type;            /* DEVIO type */
+    uint32_t smio_inst_id;              /* Optional SMIO instance ID for DEVIOs
+                                           with a single SMIO */
     char *dev_pathname;                 /* /dev pathname */
     enum _devio_state_e state;          /* Device IO state */
 };
@@ -31,7 +35,7 @@ typedef struct _devio_info_t devio_info_t;
 typedef enum _devio_state_e devio_state_e;
 
 devio_info_t * devio_info_new (char *dev_pathname, uint32_t id, llio_type_e type,
-        devio_state_e state);
+        devio_type_e devio_type, uint32_t smio_inst_id, devio_state_e state);
 dmngr_err_e devio_info_destroy (devio_info_t **self_p);
 
 #endif
