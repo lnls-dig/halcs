6be50b9a9a3f020f4945adb496d3535273d91ce3
Lucas Russo <lerwys@gmail.com>
2015-03-04 20:17:05 -0300

    examples/*: add more functionalities to examples

--- examples/fmc130m_4ch_ctl.c
+++ examples/fmc130m_4ch_ctl.c
@@ -28,11 +28,6 @@ void print_help (char *program_name)
             "\t-b <broker_endpoint> Broker endpoint\n"
             "\t-pll_status PLL Lock status\n"
             "\t-trig_dir <dir> Trigger direction\n"
-            "\t-clk_sel <sel = [0(FMC front panel)|1(FMC pin)]> Clock selection\n"
-            "\t-adc_rand <rand = [0|1]> ADC RAND\n"
-            "\t-adc_dith <dith = [0|1]> ADC DITH\n"
-            "\t-adc_shdn <shdn = [0|1]> ADC SHDN\n"
-            "\t-adc_pga  <pga = [0|1]> ADC PGA\n"
             , program_name);
 }
 
@@ -43,11 +38,7 @@ int main (int argc, char *argv [])
     char *board_number_str = NULL;
     char *bpm_number_str = NULL;
     char *trig_dir_str = NULL;
-    char *clk_sel_str = NULL;
-    char *adc_rand_str = NULL;
-    char *adc_dith_str = NULL;
-    char *adc_shdn_str = NULL;
-    char *adc_pga_str = NULL;
+    char *trig_term_str = NULL;
     uint32_t get_pll_status = 0;
     char **str_p = NULL;
 
@@ -88,20 +79,8 @@ int main (int argc, char *argv [])
         else if (streq (argv[i], "-trig_dir")) {
             str_p = &trig_dir_str;
         }
-        else if (streq (argv[i], "-clk_sel")) {
-            str_p = &clk_sel_str;
-        }
-        else if (streq (argv[i], "-adc_rand")) {
-            str_p = &adc_rand_str;
-        }
-        else if (streq (argv[i], "-adc_dith")) {
-            str_p = &adc_dith_str;
-        }
-        else if (streq (argv[i], "-adc_shdn")) {
-            str_p = &adc_shdn_str;
-        }
-        else if (streq (argv[i], "-adc_pga")) {
-            str_p = &adc_pga_str;
+        else if (streq (argv[i], "-trig_term")) {
+            str_p = &trig_term_str;
         }
         /* Fallout for options with parameters */
         else {
@@ -168,69 +147,17 @@ int main (int argc, char *argv [])
         fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_trig_dir was successfully executed\n");
     }
 
-    uint32_t clk_sel = 0;
-    if (clk_sel_str != NULL) {
-        clk_sel = strtoul (clk_sel_str, NULL, 10);
+    uint32_t trig_term = 0;
+    if (trig_term_str != NULL) {
+        trig_term = strtoul (trig_term_str, NULL, 10);
 
-        fprintf (stdout, "[client:fmc130m_4ch]: clk_sel = %u\n", clk_sel);
-        err = bpm_set_fmc_clk_sel (bpm_client, service, clk_sel);
+        fprintf (stdout, "[client:fmc130m_4ch]: trig_term = %u\n", trig_term);
+        err = bpm_set_trig_term (bpm_client, service, trig_term);
         if (err != BPM_CLIENT_SUCCESS){
-            fprintf (stderr, "[client:fmc130m_4ch]: bpm_set_fmc_clk_sel failed\n");
+            fprintf (stderr, "[client:fmc130m_4ch]: bpm_set_trig_term failed\n");
             goto err_bpm_exit;
         }
-        fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_fmc_clk_sel was successfully executed\n");
-    }
-
-    uint32_t adc_rand = 0;
-    if (adc_rand_str != NULL) {
-        adc_rand = strtoul (adc_rand_str, NULL, 10);
-
-        fprintf (stdout, "[client:fmc130m_4ch]: adc_rand = %u\n", adc_rand);
-        err = bpm_set_adc_rand (bpm_client, service, adc_rand);
-        if (err != BPM_CLIENT_SUCCESS){
-            fprintf (stderr, "[client:fmc130m_4ch]: bpm_set_adc_rand failed\n");
-            goto err_bpm_exit;
-        }
-        fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_adc_rand was successfully executed\n");
-    }
-
-    uint32_t adc_dith = 0;
-    if (adc_dith_str != NULL) {
-        adc_dith = strtoul (adc_dith_str, NULL, 10);
-
-        fprintf (stdout, "[client:fmc130m_4ch]: adc_dith = %u\n", adc_dith);
-        err = bpm_set_adc_dith (bpm_client, service, adc_dith);
-        if (err != BPM_CLIENT_SUCCESS){
-            fprintf (stderr, "[client:fmc130m_4ch]: bpm_set_adc_dith failed\n");
-            goto err_bpm_exit;
-        }
-        fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_adc_dith was successfully executed\n");
-    }
-
-    uint32_t adc_shdn = 0;
-    if (adc_shdn_str != NULL) {
-        adc_shdn = strtoul (adc_shdn_str, NULL, 10);
-
-        fprintf (stdout, "[client:fmc130m_4ch]: adc_shdn = %u\n", adc_shdn);
-        err = bpm_set_adc_shdn (bpm_client, service, adc_shdn);
-        if (err != BPM_CLIENT_SUCCESS){
-            fprintf (stderr, "[client:fmc130m_4ch]: bpm_set_adc_shdn failed\n");
-            goto err_bpm_exit;
-        }
-        fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_adc_shdn was successfully executed\n");
-    }
-
-    uint32_t adc_pga = 0;
-    if (adc_pga_str != NULL) {
-        adc_pga = strtoul (adc_pga_str, NULL, 10);
-
-        fprintf (stdout, "[client:fmc130m_4ch]: adc_pga = %u\n", adc_pga);
-        err = bpm_set_adc_pga (bpm_client, service, adc_pga);
-        if (err != BPM_CLIENT_SUCCESS){
-            fprintf (stderr, "[client:fmc130m_4ch]: bpm_set_adc_pga failed\n");
-            goto err_bpm_exit;
-        }
-        fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_adc_pga was successfully executed\n");
+        fprintf (stdout, "[client:fmc130m_4ch]: bpm_set_trig_term was successfully executed\n");
     }
 
     if (get_pll_status) {
@@ -246,18 +173,9 @@ int main (int argc, char *argv [])
 
 err_bpm_exit:
     bpm_client_destroy (&bpm_client);
-    str_p = &adc_pga_str;
+    str_p = &trig_term_str;
     free (*str_p);
-    adc_pga_str = NULL;
-    str_p = &adc_shdn_str;
-    free (*str_p);
-    adc_shdn_str = NULL;
-    str_p = &adc_dith_str;
-    free (*str_p);
-    adc_dith_str = NULL;
-    str_p = &adc_rand_str;
-    free (*str_p);
-    adc_rand_str = NULL;
+    trig_term_str = NULL;
     str_p = &trig_dir_str;
     free (*str_p);
     trig_dir_str = NULL;
