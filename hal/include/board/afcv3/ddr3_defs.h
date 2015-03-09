b26f1093949dbb992efc12c33d6207f4970b60a8
Lucas Russo <lerwys@gmail.com>
2015-03-09 13:26:32 -0400

    hal/include/board/afcv3/ddr3_defs.h: can't access DDR3 past 0x00FFFFFF
    Currently, there is a bug in the FPGA firmware in that we can't
    access DDR3 addresses past 0x00FFFFFF.
    
    This is a known issue, github #39.

--- hal/include/board/afcv3/ddr3_defs.h
+++ hal/include/board/afcv3/ddr3_defs.h
@@ -1,7 +1,7 @@
 #ifndef _DDR3_DEFS_H_
 #define _DDR3_DEFS_H_
 
-#define MEM_TOTAL_SIZE                  (1UL << 31)             /* 2 GB reserved for position storage */
+#define MEM_TOTAL_SIZE                  (1UL << 28)             /* 2 GB reserved for position storage */
 #define MEM_REGION_SIZE                 (MEM_TOTAL_SIZE / 16)
 
 /* FPGA Specific */
