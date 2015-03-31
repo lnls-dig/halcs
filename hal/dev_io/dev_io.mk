05d8ae53a4afc5cc80804b01e6086ec81e133397
Lucas Russo <lerwys@gmail.com>
2015-03-31 09:14:21 -0300

    hal/dev_io/dev_io.mk: generate dev_io_cfg only when WITH_DEVIO_CFG=y

--- hal/dev_io/dev_io.mk
+++ hal/dev_io/dev_io.mk
@@ -7,21 +7,29 @@ dev_io_DIR = hal/dev_io
 # makefile
 dev_io_core_OBJS = $(dev_io_DIR)/dev_io_core.o \
 		   $(dev_io_DIR)/dev_io_err.o \
-    	   $(dev_io_utils_OBJS)
+		   $(dev_io_utils_OBJS)
 
 dev_io_OBJS = $(dev_io_DIR)/dev_io.o
-
-dev_io_cfg_OBJS = $(dev_io_DIR)/dev_io_cfg.o
 
 dev_io_INCLUDE_DIRS = $(dev_io_DIR) \
     $(dev_io_utils_INCLUDE_DIRS) \
     $(LIBCLIENT_DIR)
 
+dev_io_OUT = dev_io
+
+ifeq ($(WITH_DEVIO_CFG),y)
+dev_io_cfg_OBJS = $(dev_io_DIR)/dev_io_cfg.o
 dev_io_cfg_INCLUDE_DIRS = $(dev_io_DIR) \
     $(dev_io_utils_INCLUDE_DIRS) \
     $(LIBCLIENT_DIR)
+dev_io_cfg_OUT = dev_io_cfg
+else
+dev_io_cfg_OBJS =
+dev_io_cfg_INCLUDE_DIRS =
+dev_io_cfg_OUT =
+endif
 
-dev_io_OUT = dev_io dev_io_cfg
+dev_io_OUT += $(dev_io_cfg_OUT)
 
-dev_io_all_OUT = $(dev_io_OUT)
+dev_io_all_OUT = dev_io dev_io_cfg
 
