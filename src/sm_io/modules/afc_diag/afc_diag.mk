a6ba4a502bba58612147362d912d1732d73e5c24
Lucas Russo <lerwys@gmail.com>
2015-05-16 17:33:57 -0300

    src/sm_io/*/afc_diag/*.mk: add common objects regardelss BOARD

--- src/sm_io/modules/afc_diag/afc_diag.mk
+++ src/sm_io/modules/afc_diag/afc_diag.mk
@@ -1,14 +1,11 @@
-ifeq ($(BOARD),afcv3)
 sm_io_afc_diag_DIR = $(SRC_DIR)/sm_io/modules/afc_diag
 
-sm_io_afc_diag_OBJS = $(sm_io_afc_diag_DIR)/sm_io_afc_diag_core.o \
+sm_io_afc_diag_OBJS = $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exports.o
+
+ifeq ($(BOARD),afcv3)
+sm_io_afc_diag_OBJS += $(sm_io_afc_diag_DIR)/sm_io_afc_diag_core.o \
 		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exp.o \
-		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exports.o \
 		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_defaults.o
+endif
 
 sm_io_afc_diag_INCLUDE_DIRS = $(sm_io_afc_diag_DIR)
-else
-sm_io_afc_diag_DIR =
-sm_io_afc_diag_OBJS =
-sm_io_afc_diag_INCLUDE_DIRS =
-endif
