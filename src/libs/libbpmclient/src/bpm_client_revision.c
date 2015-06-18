ec450d8bddd907f94c79c60580abe03df5aeb052
Lucas Russo <lerwys@gmail.com>
2015-06-18 13:46:35 -0300

    src/libs/*/*/bpm_client_revision.c: add MAJOR,MINOR,PATCH strings

--- src/libs/libbpmclient/src/bpm_client_revision.c
+++ src/libs/libbpmclient/src/bpm_client_revision.c
@@ -6,6 +6,12 @@
  */
 
 #include "bpm_client.h"
+/* Private headers */
+#include "errhand.h"
+
+#define BPM_CLIENT_VERSION_MAJOR_STR STRINGIFY(BPM_CLIENT_VERSION_MAJOR)
+#define BPM_CLIENT_VERSION_MINOR_STR STRINGIFY(BPM_CLIENT_VERSION_MINOR)
+#define BPM_CLIENT_VERSION_PATCH_STR STRINGIFY(BPM_CLIENT_VERSION_PATCH)
 
 const char *build_revision = GIT_REVISION;
 const char *build_date = __DATE__ " " __TIME__;
