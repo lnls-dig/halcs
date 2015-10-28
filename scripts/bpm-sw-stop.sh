03baa3b3862aeb96fd0c31e9ce42f1b80f27c338
Lucas Russo <lerwys@gmail.com>
2015-10-28 13:46:56 -0200

    scripts/*: readd start/stop scripts for no systemd/upstart systems

---
+++ scripts/bpm-sw-stop.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+BIN_INSTANCE=$1
+
+function usage {
+    echo "Usage: $0 <bin instance>"
+}
+
+if [ -z "$BIN_INSTANCE" ]; then
+    echo "\"BIN_INSTANCE\" variable unset."
+    usage
+    exit 1
+fi
+
+# FIXME: Find correct dev_io instance looking at all running processes
+TARGET_PID=$(ps aux | grep -Ei "dev_io.*/dev/fpga${BIN_INSTANCE}.*" | head -1 | awk '{print $2}')
+
+kill ${TARGET_PID}
