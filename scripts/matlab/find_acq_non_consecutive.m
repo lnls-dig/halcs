62489be06c5d7a0d96c41eb896c288bf70d7e09a
Lucas Russo <lerwys@gmail.com>
2016-01-29 08:47:27 -0200

    scripts/matlab/find_acq_*.m: detect false positves and discard them

--- scripts/matlab/find_acq_non_consecutive.m
+++ scripts/matlab/find_acq_non_consecutive.m
@@ -3,7 +3,8 @@ function find_acq_non_consecutive(filename)
 data=load(filename);
 
 for n = 1:size(data,2)
-    find_non_consecutive(data(:,n)')
+    nc = find_non_consecutive(data(:,n)');
+    nc_wrap = omit_wraparound(data(:,n)', nc, "twoscomplement", 16)
 end
 
 end
