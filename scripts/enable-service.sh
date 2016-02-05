075f0c1310ba88b66010e6f6ff09a000058e1f8d
Lucas Russo <lerwys@gmail.com>
2016-02-05 15:07:49 -0200

    scripts/*: add enable/disable service scripts

---
+++ scripts/enable-service.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+function usage() {
+    echo "Usage: $0 [-s <service_name>]"
+}
+
+SERVICE=
+
+# Get command line options
+while getopts ":s:" opt; do
+    case $opt in
+        d)
+            SERVICE=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+INIT_SYSTEM=$(./get-init-system.sh)
+
+# Enable service depending on which init system we are
+
+case ${INIT_SYSTEM} in
+    "systemd")
+        echo "Enabling systemd service "${SERVICE}
+        systemctl enable ${SERVICE}
+        ;;
+    "upstart")
+        echo "Enabling upstart service "${SERVICE}
+        ;;
+    "sysv-init")
+        echo "Enabling sysv-init service "${SERVICE}
+        ;;
+    "?")
+        echo "Invalid init system detected." >&2
+        exit 1
+        ;;
+esac
