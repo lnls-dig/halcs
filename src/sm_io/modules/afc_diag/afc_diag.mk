dc6c3639fe80c8fe2f4341e5568ef86bbd59bb02
Lucas Russo <lerwys@gmail.com>
2016-02-13 18:03:57 -0200

    src/*/modules/*/afc_diag.mk: re-include conditional AFC compilation
    In fact, we need this for non AFC boards,
    like ml605 which does not have AFC DIAG modules.
    Thus, no addresses for AFC DIAG module exists in the
    mem_layout.h file, causing the compilation to fail.

--- src/sm_io/modules/afc_diag/afc_diag.mk
+++ src/sm_io/modules/afc_diag/afc_diag.mk
@@ -2,7 +2,8 @@ sm_io_afc_diag_DIR = $(SRC_DIR)/sm_io/modules/afc_diag
 
 sm_io_afc_diag_OBJS = $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exports.o
 
-ifeq ($(BOARD),afcv3)
+# Only compile this for supported AFCv3 boards
+ifeq ($(BOARD),$(SUPPORTED_AFCV3_BOARDS))
 sm_io_afc_diag_OBJS += $(sm_io_afc_diag_DIR)/sm_io_afc_diag_core.o \
 		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exp.o \
 		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_defaults.o
