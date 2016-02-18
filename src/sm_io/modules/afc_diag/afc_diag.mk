ea46297d2509579e446cdf7d2293ab2e442512c8
Lucas Russo <lerwys@gmail.com>
2016-02-18 08:44:53 -0500

    sm_io/*/afc_diag/afc_diag.mk: fix board version detection
    This was effectively skipping compilation of
    AFC Diagnostics module.

--- src/sm_io/modules/afc_diag/afc_diag.mk
+++ src/sm_io/modules/afc_diag/afc_diag.mk
@@ -3,7 +3,7 @@ sm_io_afc_diag_DIR = $(SRC_DIR)/sm_io/modules/afc_diag
 sm_io_afc_diag_OBJS = $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exports.o
 
 # Only compile this for supported AFCv3 boards
-ifeq ($(BOARD),$(SUPPORTED_AFCV3_BOARDS))
+ifeq ($(BOARD),$(filter $(BOARD),$(SUPPORTED_AFCV3_BOARDS)))
 sm_io_afc_diag_OBJS += $(sm_io_afc_diag_DIR)/sm_io_afc_diag_core.o \
 		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_exp.o \
 		 $(sm_io_afc_diag_DIR)/sm_io_afc_diag_defaults.o
