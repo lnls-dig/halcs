16fed0619334d1dbfeb31492e0b661ad4a0fafec
Lucas Russo <lerwys@gmail.com>
2016-02-23 13:54:38 -0300

    various: add FMC250M support for ML605

--- src/boards/ml605/chips_addr.c
+++ src/boards/ml605/chips_addr.c
@@ -20,3 +20,17 @@ const uint32_t fmc130m_4ch_lm75a_addr[NUM_FMC130M_4CH_SMIOS][NUM_FMC130M_4CH_LM7
 /* This CI PCA9547 is located on the carrier, but it's controlled by the FMC130M_4CH */
 const uint32_t fmc130m_4ch_pca9547_addr[NUM_FMC130M_4CH_SMIOS] = {0x70};
 
+/* FMC250M_4CH chip addresses */
+const uint32_t fmc250m_4ch_si571_addr[NUM_FMC250M_4CH_SMIOS] = {0x49};
+const uint32_t fmc250m_4ch_ad9510_addr[NUM_FMC250M_4CH_SMIOS] = {0x01};
+
+/* The FMC EEPROM was supposed to have address 0x50 and 0x52, but GA1 and GA0
+ * are all grounded on hardware */
+const uint32_t fmc250m_4ch_24aa64_addr[NUM_FMC250M_4CH_SMIOS] = {0x50};
+
+const uint32_t fmc250m_4ch_amc7823_addr[NUM_FMC250M_4CH_SMIOS] = {0x01};
+const uint32_t fmc250m_4ch_isla216p_addr[NUM_FMC250M_4CH_SMIOS][NUM_FMC250M_4CH_ISLA216P] = {
+        {0x01, 0x02, 0x03, 0x04}
+};
+/* This CI PCA9547 is located on the carrier, but it's controlled by the FMC250M_4CH */
+const uint32_t fmc250m_4ch_pca9547_addr[NUM_FMC250M_4CH_SMIOS] = {0x70};
