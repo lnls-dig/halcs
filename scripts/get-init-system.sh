297885dfde0216b128ce18a8652d8c2b4d38a859
Lucas Russo <lerwys@gmail.com>
2016-03-07 15:50:50 -0300

    get-init-system.sh: poke systemd directories for chroot environ

--- scripts/get-init-system.sh
+++ scripts/get-init-system.sh
@@ -9,6 +9,10 @@ if [[ ${IS_UPSTART} =~ upstart ]]; then
     echo "upstart";
 elif [[ `systemctl` =~ -\.mount ]]; then
     echo "systemd";
+# If on chroot the above alternative will not work.
+# Try poking a few systemd directories instead
+elif [[ -f /usr/lib/systemd || -f /lib/systemd/systemd ]]; then
+    echo "systemd";
 elif [[ -f /etc/init.d/cron && ! -h /etc/init.d/cron ]]; then
     echo "sysv-init";
 else
