bab2d737fdd0c0110286d92578c2ae11981e27b2
Lucas Russo <lerwys@gmail.com>
2016-03-07 13:41:29 -0300

    scripts/get-init-system.sh: throw away error from upstart checking

--- scripts/get-init-system.sh
+++ scripts/get-init-system.sh
@@ -3,7 +3,9 @@
 # Based from the stackexchange answer
 # http://unix.stackexchange.com/questions/18209/detect-init-system-using-the-shell
 
-if [[ `/sbin/init --version` =~ upstart ]]; then
+IS_UPSTART=$(/sbin/init --version 2> /dev/null)
+
+if [[ ${IS_UPSTART} =~ upstart ]]; then
     echo "upstart";
 elif [[ `systemctl` =~ -\.mount ]]; then
     echo "systemd";
