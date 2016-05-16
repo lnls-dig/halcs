84cb96ef148441e3b95161fd1f42b36fb16267d6
Lucas Russo <lerwys@gmail.com>
2016-05-16 15:33:42 -0300

    sm_io/*: add trigger interface module to build system

--- src/sm_io/modules/modules.mk
+++ src/sm_io/modules/modules.mk
@@ -6,7 +6,8 @@ include $(SRC_DIR)/sm_io/modules/fmc130m_4ch/fmc130m_4ch.mk \
 		$(SRC_DIR)/sm_io/modules/dsp/dsp.mk \
 		$(SRC_DIR)/sm_io/modules/swap/swap.mk \
 		$(SRC_DIR)/sm_io/modules/rffe/rffe.mk \
-		$(SRC_DIR)/sm_io/modules/afc_diag/afc_diag.mk
+		$(SRC_DIR)/sm_io/modules/afc_diag/afc_diag.mk \
+		$(SRC_DIR)/sm_io/modules/trigger_iface/trigger_iface.mk
 
 sm_io_modules_DIR = $(SRC_DIR)/sm_io/modules
 
@@ -19,4 +20,5 @@ sm_io_modules_OBJS = $(sm_io_modules_DIR)/sm_io_codes.o \
 		     $(sm_io_dsp_OBJS) \
 		     $(sm_io_swap_OBJS) \
 		     $(sm_io_rffe_OBJS) \
-		     $(sm_io_afc_diag_OBJS)
+		     $(sm_io_afc_diag_OBJS) \
+		     $(sm_io_trigger_iface_OBJS)
--- src/sm_io/modules/sm_io_codes.h
+++ src/sm_io/modules/sm_io_codes.h
@@ -31,6 +31,7 @@ typedef struct _smio_rffe_version_t smio_rffe_version_t;
 #include "sm_io_swap_codes.h"
 #include "sm_io_rffe_codes.h"
 #include "sm_io_afc_diag_codes.h"
+#include "sm_io_trigger_iface_codes.h"
 
 /* Include all function descriptors */
 #include "sm_io_fmc130m_4ch_exports.h"
@@ -42,6 +43,7 @@ typedef struct _smio_rffe_version_t smio_rffe_version_t;
 #include "sm_io_swap_exports.h"
 #include "sm_io_rffe_exports.h"
 #include "sm_io_afc_diag_exports.h"
+#include "sm_io_trigger_iface_exports.h"
 
 /* Merge all function descriptors in a single structure */
 extern const disp_op_t **smio_exp_ops [];
