43330bf35fe3fea6ced427055a8352c352f5fe3a
Lucas Russo <lerwys@gmail.com>
2016-06-09 10:55:12 -0300

    sm_io/*/trigger_mux/*: default SW_CLK trigger channel to DSP CLK

--- src/sm_io/modules/trigger_mux/sm_io_trigger_mux_defaults.h
+++ src/sm_io/modules/trigger_mux/sm_io_trigger_mux_defaults.h
@@ -15,6 +15,13 @@
 #define TRIGGER_MUX_DFLT_TRANSM_SRC               0      /* Trigger Backplane */
 #define TRIGGER_MUX_DFLT_TRANSM_IN_SEL            0      /* Trigger Index 0 */
 
+/* FIXME. This shoulw be somewhere else*/
+#define TRIGGER_MUX_SW_CLK_CHAN                   17     /* Trigger Channel */
+#define TRIGGER_MUX_SW_CLK_DFLT_RCV_SRC           1      /* FPGA Internal */
+#define TRIGGER_MUX_SW_CLK_DFLT_RCV_IN_SEL        1      /* Trigger Index 1 */
+#define TRIGGER_MUX_SW_CLK_DFLT_TRANSM_SRC        0      /* FPGA Internal */
+#define TRIGGER_MUX_SW_CLK_DFLT_TRANSM_IN_SEL     0      /* Trigger Index 0 */
+
 smio_err_e trigger_mux_config_defaults (char *broker_endp, char *service,
         const char *log_file_name);
 
