eb478ed97498d405ebee163c94cd30d2b33d872d
Lucas Russo <lerwys@gmail.com>
2016-08-19 09:37:00 -0300

    scripts/change-bpm-name.sh: add more renaming targets

--- scripts/change-bpm-name.sh
+++ scripts/change-bpm-name.sh
@@ -6,14 +6,20 @@ set -a
 
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-${CURDIR}/change-proj-name.sh -o BPM-SW -n HALCS
-${CURDIR}/change-proj-name.sh -o bpm-sw -n halcs
-${CURDIR}/change-proj-name.sh -o EBPM -n HALCSD
-${CURDIR}/change-proj-name.sh -o ebpm -n halcsd
-
+# More general names first
+${CURDIR}/change-proj-name.sh -o LIBBPMCLIENT -n LIBHALCSCLIENT
+${CURDIR}/change-proj-name.sh -o libbpmclient -n libhalcsclient
 ${CURDIR}/change-proj-name.sh -o BPMCLIENT -n HALCSCLIENT
 ${CURDIR}/change-proj-name.sh -o bpmclient -n halcsclient
 ${CURDIR}/change-proj-name.sh -o BPM_CLIENT -n HALCS_CLIENT
 ${CURDIR}/change-proj-name.sh -o bpm_client -n halcs_client
-${CURDIR}/change-proj-name.sh -o LIBBPMCLIENT -n LIBHALCSCLIENT
-${CURDIR}/change-proj-name.sh -o libbpmclient -n libhalcsclient
+
+${CURDIR}/change-proj-name.sh -o BPM-SW -n HALCS
+${CURDIR}/change-proj-name.sh -o bpm-sw -n halcs
+${CURDIR}/change-proj-name.sh -o BPM_SW -n HALCS
+${CURDIR}/change-proj-name.sh -o bpm_sw -n halcs
+${CURDIR}/change-proj-name.sh -o EBPM -n HALCSD
+${CURDIR}/change-proj-name.sh -o ebpm -n halcsd
+
+${CURDIR}/change-proj-name.sh -o BPM_ -n HALCS_
+${CURDIR}/change-proj-name.sh -o bpm_ -n halcs_
