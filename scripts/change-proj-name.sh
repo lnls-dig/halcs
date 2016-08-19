4f1f39e908dfd82db4e00724e4cac4261275f270
Lucas Russo <lerwys@gmail.com>
2016-08-19 09:06:16 -0300

    scripts/change-proj-name.sh: fix script to change only one pattern at a time

--- scripts/change-proj-name.sh
+++ scripts/change-proj-name.sh
@@ -7,7 +7,7 @@ set -a
 TOP=$(pwd)
 
 function usage() {
-    echo "Usage: $0 [-o <Current project name> -n <New project name> -i <Current main app> -t <New main app>]"
+        echo "Usage: $0 [-o <Current project name> -n <New project name>]"
 }
 
 function change_filenames () {
@@ -47,28 +47,17 @@ function change_file_insides () {
     cd ${TOP}
 }
 
-LIBS_DIR=src/libs
-LIBS_CURRENT_NAME=bpm
-LIBS_NEW_NAME=
-CURRENT_MAIN_APP=
-NEW_MAIN_APP=
 CURRENT_NAME=
 NEW_NAME=
 
 # Get command line options
-while getopts ":o:n:i:t:" opt; do
+while getopts ":o:n::" opt; do
     case $opt in
         o)
             CURRENT_NAME=$OPTARG
             ;;
         n)
             NEW_NAME=$OPTARG
-            ;;
-        i)
-            CURRENT_MAIN_APP=$OPTARG
-            ;;
-        t)
-            NEW_MAIN_APP=$OPTARG
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -95,36 +84,8 @@ if [ -z "$NEW_NAME" ]; then
     exit 1
 fi
 
-
-if [ -z "$CURRENT_MAIN_APP" ]; then
-    echo "\"CURRENT_MAIN_APP\" variable unset."
-    usage
-    exit 1
-fi
-
-if [ -z "$NEW_MAIN_APP" ]; then
-    echo "\"NEW_MAIN_APP\" variable unset."
-    usage
-    exit 1
-fi
-
 # Rename project filenames
 change_filenames "${CURRENT_NAME}" "${NEW_NAME}"
 
 ## Change project file insides
 change_file_insides "${CURRENT_NAME}" "${NEW_NAME}" ""
-
-# Change LIBS filenames
-change_filenames "${LIBS_CURRENT_NAME}_" "${LIBS_NEW_NAME}" "${LIBS_DIR}"
-change_filenames "${LIBS_CURRENT_NAME}" "${LIBS_NEW_NAME}" "${LIBS_DIR}"
-
-#Change LIBS file insides
-change_file_insides "${LIBS_CURRENT_NAME}_" "${LIBS_NEW_NAME}" "${LIBS_DIR}"
-change_file_insides "${LIBS_CURRENT_NAME}" "${LIBS_NEW_NAME}" "${LIBS_DIR}"
-
-# Rename project filenames
-change_filenames "${CURRENT_MAIN_APP}" "${NEW_MAIN_APP}"
-
-## Change project file insides
-change_file_insides "${CURRENT_MAIN_APP}" "${NEW_MAIN_APP}" ""
-
