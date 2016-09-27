416a320c4e0dc83030c1e65426b1029f00af42f6
Lucas Russo <lerwys@gmail.com>
2016-09-27 13:11:03 -0300

    scripts/systemd/*: fix generate-idx script to output 3 indexes

--- scripts/systemd/usr/local/bin/generate-dev-idx.sh
+++ scripts/systemd/usr/local/bin/generate-dev-idx.sh
@@ -4,6 +4,7 @@ INPUT=$1
 
 DEV_IDX=$(echo ${INPUT} | sed 's|.*-||g')
 HALCS_IDX=$(expr ${DEV_IDX} \* 2 - 1)
+HALCS_IDX_NEXT=$(expr ${HALCS_IDX} \+ 1)
 
 # Output space-separated indexes
-echo ${DEV_IDX} ${HALCS_IDX}
+echo ${DEV_IDX} ${HALCS_IDX} ${HALCS_IDX_NEXT}
