ed14e7c5a83d26ec6f68e7c85a90947474c912d2
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-10-06 13:19:55 +0000

    gradle: create DistributionPlugin
    Make the software model aware of distributions. They can also be
    accessed through a distributions extension.

---
+++ buildSrc/src/main/groovy/br/lnls/dig/gradle/distribution/model/internal/DefaultDistributionContainer.groovy
@@ -0,0 +1,34 @@
+package br.lnls.dig.gradle.distribution.model.internal
+
+import org.gradle.api.file.CopySpec
+import org.gradle.api.distribution.Distribution
+import org.gradle.api.internal.DefaultPolymorphicDomainObjectContainer
+import org.gradle.api.internal.file.FileOperations
+import org.gradle.internal.reflect.Instantiator
+
+import br.lnls.dig.gradle.distribution.model.DistributionContainer
+
+public class DefaultDistributionContainer
+        extends DefaultPolymorphicDomainObjectContainer<Distribution>
+        implements DistributionContainer {
+    private FileOperations fileOperations
+
+    public DefaultDistributionContainer(FileOperations fileOperations,
+            Instantiator instantiator) {
+        super(Distribution, instantiator)
+
+        this.fileOperations = fileOperations
+    }
+
+    public <T extends Distribution> void registerFactory(Class<T> interfaceType,
+            Class<? extends T> instanceType) {
+        def fileOperations = this.fileOperations
+
+        registerFactory(interfaceType) { name ->
+            def constructor = instanceType.getDeclaredConstructor(String,
+                    CopySpec)
+
+            return constructor.newInstance(name, fileOperations.copySpec())
+        }
+    }
+}
