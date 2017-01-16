b4fbe324c2819b7ac2cd3524ed66ad272316128a
Lucas Russo <lerwys@gmail.com>
2017-01-16 09:33:53 -0200

    {.travis.yml,ci_deploy.sh}: pack each board RPM into tar.gz file
    This would ease downloading and usage of
    RPM packages until we have a repository
    in which we can upload our RPMs and rely
    on.

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 
+# help debug
+set -x
+
+COMPILE_MODES=()
+COMPILE_MODES+=("Debug")
+COMPILE_MODES+=("DebugDevelopment")
+COMPILE_MODES+=("Development")
+
+BOARDS=()
+BOARDS+=("afcv3")
+BOARDS+=("afcv3_1")
+BOARDS+=("ml605")
+
+TOPDIR=$(pwd)
+
 if [ "$DEPLOY" = "yes" ]; then
-    md5sum build/release/*.rpm > build/release/MD5SUMS
-    sha1sum build/release/*.rpm > build/release/SHA1SUMS
+    mkdir dist
+    export HALCS_DEPLOYMENT=dist/*
+
+    # Build put our deploy targets into build/releases.
+    # Pack each target in a single tar.gz
+    cd build/release
+    for board in "${BOARDS[@]}"
+    do
+        for mode in "${COMPILE_MODES[@]}"
+        do
+            tar cvzf ${board}${mode}.tar.gz ${board}${mode}-* || exit 1
+            mv ${board}${mode}.tar.gz ${TOPDIR}/dist
+        done
+    done
+    cd -
+
+    # Generate hash sums
+    cd dist
+    md5sum ${TOPDIR}/build/release/*.rpm > MD5SUMS
+    sha1sum ${TOPDIR}/build/release/*.rpm > SHA1SUMS
+    cd -
 fi
