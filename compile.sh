8b4b6b1ed422eef41aeb4c22b80cdd406078587a
Lucas Russo <lerwys@gmail.com>
2017-01-16 08:23:48 -0200

    {Makefile,compile.sh}: build changes for omnisys crate

--- compile.sh
+++ compile.sh
@@ -108,7 +108,7 @@ LOCAL_MSG_DBG=n
 #Select if we want to compile with debug mode on. Options are: y(es) or n(o)
 ERRHAND_DBG=y
 # Select the minimum debug verbosity. See liberrhand file errhand_opts.h for more info.
-ERRHAND_MIN_LEVEL=DBG_LVL_WARN
+ERRHAND_MIN_LEVEL=DBG_LVL_TRACE
 # Select the subsytems which will have the debug on. See liberrhand file errhand_opts.h for more info.
 ERRHAND_SUBSYS_ON='"(DBG_DEV_MNGR | DBG_DEV_IO | DBG_SM_IO | DBG_LIB_CLIENT | DBG_SM_PR | DBG_SM_CH | DBG_LL_IO | DBG_HAL_UTILS)"'
 # Select the FMC ADC board type. Options are: passive or active
