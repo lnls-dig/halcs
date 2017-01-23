8929f020da6327cb89b0734ac287a2704c7dfaed
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-01-23 12:49:25 +0000

    gradle_compile.sh: create equivalent of compile.sh
    Closes #136

---
+++ gradle_compile.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+
+VALID_BOARDS_STR="Valid values are: \"ml605\", \"afcv3\" or \"afcv3_1\"."
+VALID_APPS_STR="Valid values are: \"halcsd\"."
+VALID_WITH_EXAMPLES_STR="Valid values are: \"yes\" or \"no\"."
+VALID_WITH_SYSTEM_INTEGRATION_STR="Valid values are: \"yes\" or \"no\"."
+VALID_WITH_DRIVER_STR="Valid values are: \"yes\" or \"no\"."
+VALID_COMPACT_INSTALL_STR="Valid values are: \"yes\" or \"no\"."
+
+function usage() {
+    echo "Usage: $0 [-b <board>] [-a <applications>] [-e <with examples = yes/no>]"
+    echo "    [-l <with library linking = yes/no>] [-d <with driver = yes/no>]"
+    echo "    [-c <compact install = yes/no>] [-x <extra flags>]"
+}
+
+#######################################
+# All of our Makefile options
+#######################################
+
+BOARD=
+APPS=
+WITH_EXAMPLES="no"
+WITH_SYSTEM_INTEGRATION="yes"
+WITH_DRIVER="no"
+COMPACT_INSTALL="no"
+EXTRA_FLAGS=
+
+# Get command line options
+while getopts ":b:a:e:l:x:d:c:" opt; do
+    case $opt in
+        b)
+            BOARD=$OPTARG
+            ;;
+        a)
+            APPS=$OPTARG
+            ;;
+        e)
+            WITH_EXAMPLES=$OPTARG
+            ;;
+        l)
+            WITH_SYSTEM_INTEGRATION=$OPTARG
+            ;;
+        x)
+            EXTRA_FLAGS=$OPTARG
+            ;;
+        d)
+            WITH_DRIVER=$OPTARG
+            ;;
+        c)
+            COMPACT_INSTALL=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$BOARD"  ]; then
+    echo "\"board\" variable unset."
+    usage
+    exit 1
+fi
+
+if [ "$BOARD" != "afcv3" ] && [ "$BOARD" != "afcv3_1" ] && [ "$BOARD" != "ml605" ]; then
+    echo "Unsupported board. "$VALID_BOARDS_STR
+    exit 1
+fi
+
+if [ -z "$APPS"  ]; then
+    echo "\"applications\" variable unset."
+    usage
+    exit 1
+fi
+
+if [ -z "$WITH_EXAMPLES"  ]; then
+    echo "\"examples\" variable unset."
+    usage
+    exit 1
+fi
+
+if [ "$WITH_EXAMPLES" != "yes" ] && [ "$WITH_EXAMPLES" != "no" ]; then
+    echo "Unsupported examples. "$VALID_WITH_EXAMPLES_STR
+    exit 1
+fi
+
+if [ "$WITH_DRIVER" != "yes" ] && [ "$WITH_DRIVER" != "no" ]; then
+    echo "Unsupported driver option. "$VALID_WITH_DRIVER_STR
+    exit 1
+fi
+
+if [ "$COMPACT_INSTALL" != "yes" ] && [ "$COMPACT_INSTALL" != "no" ]; then
+    echo "Unsupported compact install option. "$VALID_COMPACT_INSTALL_STR
+    exit 1
+fi
+
+TASKS="clean :apps:${APPS}:${BOARD}InstallRpm"
+
+if [ "$WITH_EXAMPLES" = "yes" ]; then
+    TASKS="$TASKS :examples:assemble"
+fi
+
+if [ "$WITH_SYSTEM_INTEGRATION" = "no" ]; then
+    echo "Currently unable to install without system integration."
+    exit 1
+fi
+
+if [ "$WITH_DRIVER" = "yes" ]; then
+    make ${EXTRA_FLAGS} pcie_driver pcie_driver_install || exit 1
+fi
+
+for lib in $(find libs -maxdepth 1 -mindepth 1 -type d); do
+    LIB="$(echo $lib | sed -e 's|libs/||')"
+
+    if [ "$COMPACT_INSTALL" = "yes" ]; then
+        TASKS="$TASKS :libs:${LIB}:${BOARD}InstallRpm"
+    else
+        TASKS="$TASKS :libs:${LIB}:${BOARD}DevelopmentInstallRpm"
+    fi
+done
+
+./gradlew $TASKS
+
+if [ "$WITH_SYSTEM_INTEGRATION" = "yes" ]; then
+    ldconfig
+fi
