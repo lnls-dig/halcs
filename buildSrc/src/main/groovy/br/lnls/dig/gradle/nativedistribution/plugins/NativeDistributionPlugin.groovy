90ea9fd5d66bf84d4e7efce9222266a8d5655a66
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-01-24 18:22:21 +0000

    gradle: create DefaultNativeDistributionsPlugin
    Separate from NativeDistributionPlugin. Ideally, the
    NativeDistributionPlugin should by an entry point applying all the
    internal plugins. This is the fist step in that direction.

--- buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/plugins/NativeDistributionPlugin.groovy
+++ buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/plugins/NativeDistributionPlugin.groovy
@@ -3,31 +3,18 @@ package br.lnls.dig.gradle.nativedistribution.plugins
 import org.gradle.api.distribution.Distribution
 import org.gradle.api.internal.resolve.ProjectModelResolver
 import org.gradle.api.Plugin
-import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.internal.service.ServiceRegistry
 import org.gradle.model.Each
-import org.gradle.model.Finalize
 import org.gradle.model.ModelMap
 import org.gradle.model.Mutate
 import org.gradle.model.Path
 import org.gradle.model.RuleSource
-import org.gradle.nativeplatform.BuildType
-import org.gradle.nativeplatform.BuildTypeContainer
-import org.gradle.nativeplatform.Flavor
-import org.gradle.nativeplatform.FlavorContainer
-import org.gradle.nativeplatform.NativeBinarySpec
-import org.gradle.nativeplatform.NativeLibrarySpec
-import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
-import org.gradle.nativeplatform.TargetedNativeComponent
-import org.gradle.platform.base.Platform
-import org.gradle.platform.base.PlatformContainer
 
 import br.lnls.dig.gradle.distribution.plugins.DistributionPlugin
 import br.lnls.dig.gradle.distribution.model.DistributionContainer
-import br.lnls.dig.gradle.nativedistribution.model.DistributionVariant
-import br.lnls.dig.gradle.nativedistribution.plugins.internal.DistributionVariants
+import br.lnls.dig.gradle.nativedistribution.plugins.DefaultNativeDistributionsPlugin
 import br.lnls.dig.gradle.nativedistribution.tasks.Rpm
 import br.lnls.dig.gradle.nativedistribution.tasks.RpmInstall
 
@@ -36,61 +23,10 @@ import static org.gradle.api.distribution.plugins.DistributionPlugin.MAIN_DISTRI
 class NativeDistributionPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.pluginManager.apply(DistributionPlugin)
+        project.pluginManager.apply(DefaultNativeDistributionsPlugin)
     }
 
     public static class Rules extends RuleSource {
-        @Finalize
-        public void addCurrentPlatformIfNoPlatformsWereDefined(
-                PlatformContainer platforms) {
-            if (platforms.isEmpty())
-                platforms.add(new DefaultNativePlatform("current"))
-        }
-
-        @Mutate
-        public void setTargetPlatformOfNativeComponentsIfNecessary(
-                @Each TargetedNativeComponent component) {
-            if (component.targetPlatforms.isEmpty())
-                component.targetPlatform 'current'
-        }
-
-        @Mutate
-        public void addDistributionsForBuildVariations(
-                DistributionContainer distributions,
-                BuildTypeContainer buildTypes, PlatformContainer platforms,
-                FlavorContainer flavors,
-                ModelMap<NativeLibrarySpec> nativeLibraries) {
-            def hasLibraries = !nativeLibraries.isEmpty()
-            def usages = getUsageVariations(hasLibraries)
-
-            removeMainDistribution(distributions)
-
-            DistributionVariants.from(buildTypes, platforms, flavors, usages)
-                .addTo(distributions)
-        }
-
-        private String[] getUsageVariations(boolean hasLibraries) {
-            if (hasLibraries)
-                return ["", "development"].toArray()
-            else
-                return [""].toArray()
-        }
-
-        private void removeMainDistribution(
-                DistributionContainer distributions) {
-            distributions.removeAll {
-                it.name == MAIN_DISTRIBUTION_NAME
-            }
-        }
-
-        @Mutate
-        public void addNativeBinariesToDistribution(
-                DistributionContainer distributions,
-                @Path("binaries") ModelMap<NativeBinarySpec> binaries) {
-            distributions.all { distribution ->
-                distribution.addBinaries(binaries)
-            }
-        }
-
         @Mutate
         public void addNativeDistributionTasks(ModelMap<Task> tasks,
                 DistributionContainer distributions,
---
+++ buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/plugins/internal/DefaultNativeDistributionsPlugin.groovy
@@ -0,0 +1,74 @@
+package br.lnls.dig.gradle.nativedistribution.plugins
+
+import org.gradle.model.Each
+import org.gradle.model.Finalize
+import org.gradle.model.ModelMap
+import org.gradle.model.Mutate
+import org.gradle.model.Path
+import org.gradle.model.RuleSource
+import org.gradle.nativeplatform.BuildTypeContainer
+import org.gradle.nativeplatform.FlavorContainer
+import org.gradle.nativeplatform.NativeBinarySpec
+import org.gradle.nativeplatform.NativeLibrarySpec
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+import org.gradle.nativeplatform.TargetedNativeComponent
+import org.gradle.platform.base.PlatformContainer
+
+import br.lnls.dig.gradle.distribution.model.DistributionContainer
+import br.lnls.dig.gradle.nativedistribution.plugins.internal.DistributionVariants
+
+import static org.gradle.api.distribution.plugins.DistributionPlugin.MAIN_DISTRIBUTION_NAME
+
+class DefaultNativeDistributionsPlugin extends RuleSource {
+    @Finalize
+    public void addCurrentPlatformIfNoPlatformsWereDefined(
+            PlatformContainer platforms) {
+        if (platforms.isEmpty())
+            platforms.add(new DefaultNativePlatform("current"))
+    }
+
+    @Mutate
+    public void setTargetPlatformOfNativeComponentsIfNecessary(
+            @Each TargetedNativeComponent component) {
+        if (component.targetPlatforms.isEmpty())
+            component.targetPlatform 'current'
+    }
+
+    @Mutate
+    public void addDistributionsForBuildVariations(
+            DistributionContainer distributions,
+            BuildTypeContainer buildTypes, PlatformContainer platforms,
+            FlavorContainer flavors,
+            ModelMap<NativeLibrarySpec> nativeLibraries) {
+        def hasLibraries = !nativeLibraries.isEmpty()
+        def usages = getUsageVariations(hasLibraries)
+
+        removeMainDistribution(distributions)
+
+        DistributionVariants.from(buildTypes, platforms, flavors, usages)
+            .addTo(distributions)
+    }
+
+    private String[] getUsageVariations(boolean hasLibraries) {
+        if (hasLibraries)
+            return ["", "development"].toArray()
+        else
+            return [""].toArray()
+    }
+
+    private void removeMainDistribution(
+            DistributionContainer distributions) {
+        distributions.removeAll {
+            it.name == MAIN_DISTRIBUTION_NAME
+        }
+    }
+
+    @Mutate
+    public void addNativeBinariesToDistribution(
+            DistributionContainer distributions,
+            @Path("binaries") ModelMap<NativeBinarySpec> binaries) {
+        distributions.all { distribution ->
+            distribution.addBinaries(binaries)
+        }
+    }
+}
