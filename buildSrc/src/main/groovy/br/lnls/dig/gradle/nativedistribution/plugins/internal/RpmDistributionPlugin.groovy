13551492d2bcc49c971e6e4ce20376a55f376f39
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-01-27 10:55:36 +0000

    gradle: use Distribution.taskNameFor in rules
    Remove duplicated function.

--- buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/plugins/internal/RpmDistributionPlugin.groovy
+++ buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/plugins/internal/RpmDistributionPlugin.groovy
@@ -1,9 +1,6 @@
 package br.lnls.dig.gradle.nativedistribution.plugins
 
-import org.gradle.api.distribution.Distribution
-import org.gradle.api.internal.resolve.ProjectModelResolver
 import org.gradle.api.Task
-import org.gradle.internal.service.ServiceRegistry
 import org.gradle.model.Each
 import org.gradle.model.ModelMap
 import org.gradle.model.Mutate
@@ -14,15 +11,13 @@ import br.lnls.dig.gradle.distribution.model.DistributionContainer
 import br.lnls.dig.gradle.nativedistribution.tasks.Rpm
 import br.lnls.dig.gradle.nativedistribution.tasks.RpmInstall
 
-import static org.gradle.api.distribution.plugins.DistributionPlugin.MAIN_DISTRIBUTION_NAME
-
 class RpmDistributionPlugin extends RuleSource {
     @Mutate
     public void addNativeDistributionTasks(ModelMap<Task> tasks,
             DistributionContainer distributions,
             @Path("buildDir") File buildDir) {
         distributions.all { distribution ->
-            tasks.create(getTaskNameFor(distribution, "distRpm"), Rpm) {
+            tasks.create(distribution.taskNameFor("distRpm"), Rpm) {
                     task ->
                 task.distribution = distribution
                 task.dependsOn(distribution.buildTasks)
@@ -31,14 +26,6 @@ class RpmDistributionPlugin extends RuleSource {
                         "/distributions/$distribution.name")
             }
         }
-    }
-
-    private String getTaskNameFor(Distribution distribution,
-            String taskSuffix) {
-        if (distribution.name == MAIN_DISTRIBUTION_NAME)
-            return taskSuffix
-        else
-            return distribution.name + taskSuffix.capitalize()
     }
 
     @Mutate
@@ -68,9 +55,9 @@ class RpmDistributionPlugin extends RuleSource {
             DistributionContainer distributions,
             @Path("buildDir") File buildDir) {
         distributions.all { distribution ->
-            tasks.create(getTaskNameFor(distribution, "installRpm"),
+            tasks.create(distribution.taskNameFor("installRpm"),
                     RpmInstall) { task ->
-                task.dependsOn getTaskNameFor(distribution, "distRpm")
+                task.dependsOn distribution.taskNameFor("distRpm")
                 task.distribution = distribution
                 task.rpmDirectory = new File(buildDir,
                         "/distributions/$task.distribution.name")
