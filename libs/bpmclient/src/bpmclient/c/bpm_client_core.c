7066ed214f3707b4c88cb8389b711db9f33962af
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-03-06 12:45:04 +0000

    libbpmclient: change offset sign convention

--- libs/bpmclient/src/bpmclient/c/bpm_client_core.c
+++ libs/bpmclient/src/bpmclient/c/bpm_client_core.c
@@ -292,9 +292,9 @@ static void _calculate_bpm_sample (bpm_parameters_t *parameters, double a,
     double offset_y = parameters->offset_y;
     double offset_q = parameters->offset_q;
 
-    sample->x = (uint32_t) (kx * (a - b - c + d) / sum + offset_x);
-    sample->y = (uint32_t) (ky * (a + b - c - d) / sum + offset_y);
-    sample->q = (uint32_t) (kq * (a - b + c - d) / sum + offset_q);
+    sample->x = (uint32_t) (kx * (a - b - c + d) / sum - offset_x);
+    sample->y = (uint32_t) (ky * (a + b - c - d) / sum - offset_y);
+    sample->q = (uint32_t) (kq * (a - b + c - d) / sum - offset_q);
     sample->sum = (uint32_t) (ksum * sum);
 
     DBE_DEBUG (DBG_LIB_CLIENT | DBG_LVL_TRACE, "[libbpmclient] "
