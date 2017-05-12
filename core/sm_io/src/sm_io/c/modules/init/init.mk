de563da4f5a91ddae3c1be80b94581309e7d6f69
Lucas Russo <lerwys@gmail.com>
2017-05-12 15:00:31 -0300

    core/sm_io/*/init: remove unexisting object from makfile

--- core/sm_io/src/sm_io/c/modules/init/init.mk
+++ core/sm_io/src/sm_io/c/modules/init/init.mk
@@ -1,5 +1,4 @@
 sm_io_init_DIR = $(sm_io_modules_DIR)/init
 
 sm_io_init_OBJS = $(sm_io_init_DIR)/sm_io_init_core.o \
-		 $(sm_io_init_DIR)/sm_io_init_exp.o \
-		 $(sm_io_init_DIR)/sm_io_init_defaults.o
+		 $(sm_io_init_DIR)/sm_io_init_exp.o
