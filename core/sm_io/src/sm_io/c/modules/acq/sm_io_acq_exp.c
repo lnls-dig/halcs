3ec249a5be689681958f800af4decc7de6d45983
Lucas Russo <lerwys@gmail.com>
2017-06-15 12:37:43 -0300

    sm_io/*/acq: fix minimum number os post_samples when triggered acquisitions
    This is related to github issue lnls-dig/bpm-gw#62

--- core/sm_io/src/sm_io/c/modules/acq/sm_io_acq_exp.c
+++ core/sm_io/src/sm_io/c/modules/acq/sm_io_acq_exp.c
@@ -161,14 +161,16 @@ static int _acq_data_acquire (void *owner, void *args, void *ret)
 
     /* FIXME FPGA Firmware requires number of samples to be divisible by
      * acquisition channel sample size */
+    uint32_t channel_sample_size = acq->acq_buf[chan].sample_size;
     uint32_t samples_alignment =
-        DDR3_PAYLOAD_SIZE/acq->acq_buf[chan].sample_size;
+        DDR3_PAYLOAD_SIZE/channel_sample_size;
     uint32_t num_samples_pre_aligned = hutils_align_value (num_samples_pre,
             samples_alignment);
     /* FIXME. Curently, the FPGA gateware does not support triggered acquisitions with
-     * post_samples = 0. See github lnls-bpm/bpm-gw#62 */
+     * post_samples = 0. There must be at more than DDR3_PAYLOAD_SIZE bytes in post_samples.
+     * See github lnls-bpm/bpm-gw#62 */
     uint32_t num_samples_post_aligned = (num_samples_post == 0 && trigger_type != TYPE_ACQ_CORE_SKIP) ?
-            samples_alignment : hutils_align_value (num_samples_post, samples_alignment);
+            samples_alignment*channel_sample_size + 1 : hutils_align_value (num_samples_post, samples_alignment);
 
     /* Set the parameters: number of samples of this channel */
     acq->acq_params[chan].num_samples_pre = num_samples_pre_aligned;
