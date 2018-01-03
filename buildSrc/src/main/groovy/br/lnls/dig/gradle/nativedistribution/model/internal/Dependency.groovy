2ef9adedc5ddfb1bb0b73f03242fde0095c075db
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-01-03 15:34:03 +0000

    gradle: remove projectModel field from Dependency

--- buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/model/internal/Dependency.groovy
+++ buildSrc/src/main/groovy/br/lnls/dig/gradle/nativedistribution/model/internal/Dependency.groovy
@@ -1,19 +1,16 @@
 package br.lnls.dig.gradle.nativedistribution.model.internal
 
 import org.gradle.api.distribution.Distribution
-import org.gradle.model.internal.registry.ModelRegistry
 
 class Dependency implements Serializable {
     Distribution distribution
-    ModelRegistry projectModel
     String projectPath
     String projectVersion
 
     Dependency(String projectPath, String projectVersion,
-            ModelRegistry projectModel, Distribution distribution) {
+            Distribution distribution) {
         this.projectPath = projectPath
         this.projectVersion = projectVersion
-        this.projectModel = projectModel
         this.distribution = distribution
     }
 
