4366746074b315e6e38c2c323e41bdba0a13e4aa
Lucas Russo <lerwys@gmail.com>
2018-01-04 15:07:23 -0200

    gradle_update_ver_macros.sh: add brackets to script options description

--- gradle_update_ver_macros.sh
+++ gradle_update_ver_macros.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "major" ] || [ "$1" = "minor" ] || [ "$1" = "patch" ]; then
     scope="$1"
 else
-    echo "USAGE: ./gradle_update_ver_macros.sh major|minor|patch" >&2
+    echo "USAGE: ./gradle_update_ver_macros.sh [major|minor|patch]" >&2
 fi
 
 ./gradlew updateVersionMacros -Prelease.scope="$scope" -Prelease.stage=ci_release
