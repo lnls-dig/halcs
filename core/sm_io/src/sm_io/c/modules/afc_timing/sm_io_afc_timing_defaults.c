01ddfca2b59bccaf58f4509622ca814478bf65da
Joao L de Brito Neto <joao.brito@lnls.br>
2018-06-26 14:10:51 -0300

    Add DIR wishbone registers to AFC, FMC1 and FMC2 channels. Update boot default values.

--- core/sm_io/src/sm_io/c/modules/afc_timing/sm_io_afc_timing_defaults.c
+++ core/sm_io/src/sm_io/c/modules/afc_timing/sm_io_afc_timing_defaults.c
@@ -48,155 +48,75 @@ smio_err_e afc_timing_config_defaults (char *broker_endp, char *service,
     UNUSED(log_file_name);
     DBE_DEBUG (DBG_SM_IO | DBG_LVL_INFO, "[sm_io:afc_timing_defaults] Configuring SMIO "
             "AFC_TIMING with default values ...\n");
-//    halcs_client_err_e client_err = HALCS_CLIENT_SUCCESS;
+    halcs_client_err_e client_err = HALCS_CLIENT_SUCCESS;
     smio_err_e err = SMIO_SUCCESS;
 
     halcs_client_t *config_client = halcs_client_new_log_mode (broker_endp, 0,
             log_file_name, SMIO_AFC_TIMING_LIBHALCSCLIENT_LOG_MODE);
     ASSERT_ALLOC(config_client, err_alloc_client);
-/*
-    client_err = afc_timing_set_evt_code_0 (config_client, service, AFC_TIMING_DFLT_EVT_IN0);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 0 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_evt_code_1 (config_client, service, AFC_TIMING_DFLT_EVT_IN1);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 1 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_code_2 (config_client, service, AFC_TIMING_DFLT_EVT_IN2);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 2 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_code_3 (config_client, service, AFC_TIMING_DFLT_EVT_IN3);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 3 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_code_4 (config_client, service, AFC_TIMING_DFLT_EVT_IN4);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 4 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_code_5 (config_client, service, AFC_TIMING_DFLT_EVT_IN5);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 5 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_code_6 (config_client, service, AFC_TIMING_DFLT_EVT_IN6);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 6 event code",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_0 (config_client, service, AFC_TIMING_DFLT_EVT_DLY0);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 0 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_1 (config_client, service, AFC_TIMING_DFLT_EVT_DLY1);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 1 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_2 (config_client, service, AFC_TIMING_DFLT_EVT_DLY2);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 2 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_3 (config_client, service, AFC_TIMING_DFLT_EVT_DLY3);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 3 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_4 (config_client, service, AFC_TIMING_DFLT_EVT_DLY4);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 4 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_5 (config_client, service, AFC_TIMING_DFLT_EVT_DLY5);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 5 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_delay_6 (config_client, service, AFC_TIMING_DFLT_EVT_DLY6);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 6 event delay",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_0 (config_client, service, AFC_TIMING_DFLT_EVT_WDT0);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 0 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_1 (config_client, service, AFC_TIMING_DFLT_EVT_WDT1);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 1 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_2 (config_client, service, AFC_TIMING_DFLT_EVT_WDT2);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 2 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_3 (config_client, service, AFC_TIMING_DFLT_EVT_WDT3);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 3 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_4 (config_client, service, AFC_TIMING_DFLT_EVT_WDT4);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 4 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_5 (config_client, service, AFC_TIMING_DFLT_EVT_WDT5);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 5 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_evt_width_6 (config_client, service, AFC_TIMING_DFLT_EVT_WDT6);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set channel 6 event width",
-            err_param_set, SMIO_ERR_CONFIG_DFLT);
-
-    client_err = afc_timing_set_freq_kp (config_client, service, AFC_TIMING_DFLT_FREQ_KP);
+    client_err = afc_timing_set_afc_freq_kp (config_client, service, AFC_TIMING_DFLT_AFC_FREQ_KP);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set proportional gain of frequency feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_freq_ki (config_client, service, AFC_TIMING_DFLT_FREQ_KI);
+    client_err = afc_timing_set_afc_freq_ki (config_client, service, AFC_TIMING_DFLT_AFC_FREQ_KI);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set integral gain of frequency feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_phase_kp (config_client, service, AFC_TIMING_DFLT_PHASE_KP);
+    client_err = afc_timing_set_afc_phase_kp (config_client, service, AFC_TIMING_DFLT_AFC_PHASE_KP);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set proportional gain of phase feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_phase_ki (config_client, service, AFC_TIMING_DFLT_PHASE_KI);
+    client_err = afc_timing_set_afc_phase_ki (config_client, service, AFC_TIMING_DFLT_AFC_PHASE_KI);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set integral gain of phase feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_phase_set (config_client, service, AFC_TIMING_DFLT_PHASE_SET);
+    client_err = afc_timing_set_afc_phase_set (config_client, service, AFC_TIMING_DFLT_AFC_PHASE_SET);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set set of phase feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_DDMTD_AVG_EXP);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set average exponent number of phase feedback",
+    client_err = afc_timing_set_afc_phase_navg (config_client, service, AFC_TIMING_DFLT_AFC_PHASE_NAVG);
+    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set set of phase feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_RTM_RFREQ_HI);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set RTM Si570 rfreq[37-19]",
+    client_err = afc_timing_set_afc_phase_div_exp (config_client, service, AFC_TIMING_DFLT_AFC_PHASE_DIV);
+    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set set of phase feedback",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_RTM_RFREQ_LO);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set RTM Si570 rfreq[18-0]",
+    client_err = afc_timing_set_rtm_rfreq_hi (config_client, service, AFC_TIMING_DFLT_RTM_RFREQ_HI);
+    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set RTM Si570 rfreq[37-20]",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_RTM_N1);
+    client_err = afc_timing_set_rtm_rfreq_lo (config_client, service, AFC_TIMING_DFLT_RTM_RFREQ_LO);
+    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set RTM Si570 rfreq[19-0]",
+            err_param_set, SMIO_ERR_CONFIG_DFLT);
+
+    client_err = afc_timing_set_rtm_rfreq_n1 (config_client, service, AFC_TIMING_DFLT_RTM_N1);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set RTM Si570 n1",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_RTM_HS_DIV);
+    client_err = afc_timing_set_rtm_rfreq_hs_div (config_client, service, AFC_TIMING_DFLT_RTM_HS_DIV);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set RTM Si570 hs_div",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_AFC_RFREQ_HI);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set AFC Si570 rfreq[37-19]",
+    client_err = afc_timing_set_afc_rfreq_hi (config_client, service, AFC_TIMING_DFLT_AFC_RFREQ_HI);
+    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set AFC Si570 rfreq[37-20]",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_AFC_RFREQ_LO);
-    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set AFC Si570 rfreq[18-0]",
+    client_err = afc_timing_set_afc_rfreq_lo (config_client, service, AFC_TIMING_DFLT_AFC_RFREQ_LO);
+    ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set AFC Si570 rfreq[19-0]",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_AFC_N1);
+    client_err = afc_timing_set_afc_rfreq_n1 (config_client, service, AFC_TIMING_DFLT_AFC_N1);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set AFC Si570 n1",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
-    client_err = afc_timing_set_avg_exponent (config_client, service, AFC_TIMING_DFLT_AFC_HS_DIV);
+    client_err = afc_timing_set_afc_rfreq_hs_div (config_client, service, AFC_TIMING_DFLT_AFC_HS_DIV);
     ASSERT_TEST(client_err == HALCS_CLIENT_SUCCESS, "Could not set AFC Si570 hs_div",
             err_param_set, SMIO_ERR_CONFIG_DFLT);
 
 err_param_set:
-    halcs_client_destroy (&config_client);*/
+    halcs_client_destroy (&config_client);
 err_alloc_client:
     DBE_DEBUG (DBG_SM_IO | DBG_LVL_INFO, "[sm_io:afc_timing_defaults] Exiting Config thread %s\n",
         service);
