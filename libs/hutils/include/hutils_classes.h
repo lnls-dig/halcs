60f08d812b03242e7b57d1f2f276c3c182aaacee
Lucas Russo <lerwys@gmail.com>
2019-04-01 15:00:04 -0300

    various: update minor revision number to match upcoming tag

--- libs/hutils/include/hutils_classes.h
+++ libs/hutils/include/hutils_classes.h
@@ -18,7 +18,7 @@
 /* version macros for compile-time API detection */
 
 #define HUTILS_VERSION_MAJOR 1
-#define HUTILS_VERSION_MINOR 5
+#define HUTILS_VERSION_MINOR 7
 #define HUTILS_VERSION_PATCH 0
 
 #define HUTILS_MAKE_VERSION(major, minor, patch) \
