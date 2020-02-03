84b21f1c454b7bb8376cd622e8b5c169e0d02927
Lucas Russo <lerwys@gmail.com>
2020-02-03 09:06:51 -0300

    various: prepare version number for next release v1.9.0

--- libs/bpmclient/include/bpm_client_classes.h
+++ libs/bpmclient/include/bpm_client_classes.h
@@ -22,8 +22,8 @@
 /* version macros for compile-time API detection */
 
 #define BPM_CLIENT_VERSION_MAJOR 1
-#define BPM_CLIENT_VERSION_MINOR 8
-#define BPM_CLIENT_VERSION_PATCH 2
+#define BPM_CLIENT_VERSION_MINOR 9
+#define BPM_CLIENT_VERSION_PATCH 0
 
 #define BPM_CLIENT_MAKE_VERSION(major, minor, patch) \
     ((major) * 10000 + (minor) * 100 + (patch))
