283fcbe24968c9c55b19e8e68be7bb110bdc6eb3
Lucas Russo <lerwys@gmail.com>
2020-03-10 08:33:30 -0300

    various: prepare version number for next release v1.10.0

--- libs/hutils/include/hutils_classes.h
+++ libs/hutils/include/hutils_classes.h
@@ -18,8 +18,8 @@
 /* version macros for compile-time API detection */
 
 #define HUTILS_VERSION_MAJOR 1
-#define HUTILS_VERSION_MINOR 9
-#define HUTILS_VERSION_PATCH 1
+#define HUTILS_VERSION_MINOR 10
+#define HUTILS_VERSION_PATCH 0
 
 #define HUTILS_MAKE_VERSION(major, minor, patch) \
     ((major) * 10000 + (minor) * 100 + (patch))
