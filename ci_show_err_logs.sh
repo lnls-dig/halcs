10cbb13c5decbc425e6ccf9a7cc71e4236e6af5d
Lucas Russo <lerwys@gmail.com>
2020-04-27 14:31:19 -0300

    .travis.yml,ci_show_err_logs.sh: show CMake logs if present

---
+++ ci_show_err_logs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# help debug
+set -x
+set -e
+set -o
+
+if [ -n "$CPACK" ]; then
+    # Show logs if present
+    CPACK_LOGS=$(find build/_CPack_Packages/Linux/DEB -iname "*.log")
+    for log in "${CPACK_LOGS}"; do
+        echo "Showing log ${log}:"
+        echo "------------------------------------------------------"
+        cat ${log}
+        echo "------------------------------------------------------"
+    done
+fi
