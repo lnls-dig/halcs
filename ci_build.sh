8af0a319cb081fc5f5f9d65874cbe58bf17af706
Lucas Russo <lerwys@gmail.com>
2020-04-27 11:37:25 -0300

    ci: add cmake build to travis CI

--- ci_build.sh
+++ ci_build.sh
@@ -324,8 +324,14 @@ if [ "$PURE_MAKE" = yes ]; then
 
 elif [ "$COMPILE_SCRIPT" = yes ]; then
     build-wrapper-linux-x86-64 --out-dir bw-output ./compile.sh -b $BOARD -a "${APP}" -e $EXAMPLES -l $SYSTEM_INTEGRATION -x "${HALCS_OPTS[*]}"
+elif [ "$CMAKE" = yes ]; then
+    build-wrapper-linux-x86-64 --out-dir bw-output mkdir -p build && cd build && cmake -Dhalcs_BOARD_OPT="$BOARD" ../ && make VERBOSE=1 && make DESTDIR="${BUILD_PREFIX}" install
+elif [ "$CPACK" = yes ]; then
+    for board in ${CPACK_BOARDS}; do
+        build-wrapper-linux-x86-64 --out-dir bw-output mkdir -p build && cd build && cmake -Dhalcs_BOARD_OPT="$board" ../ && cpack -G "${CPACK_GENERATORS}"
+    done
 else
-    build-wrapper-linux-x86-64 --out-dir bw-output ./compile.sh -b $BOARD -a "${APP}" -e $EXAMPLES -l $SYSTEM_INTEGRATION -x "${HALCS_OPTS[*]}"
+    build-wrapper-linux-x86-64 --out-dir bw-output mkdir -p build && cd build && cmake -Dhalcs_BOARD_OPT="$BOARD" ../ && make VERBOSE=1 && make DESTDIR="${BUILD_PREFIX}" install
 fi
 
 # Get CCache statistics
