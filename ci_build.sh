ebe3429922866ec5fe9308582ddc155ad8419979
Lucas Russo <lerwys@gmail.com>
2020-04-28 11:48:17 -0300

    ci_build.sh: build deps from sources only if not available in pre-built packages

--- ci_build.sh
+++ ci_build.sh
@@ -175,138 +175,148 @@ if [ "$HAVE_CCACHE" = yes ] && [ "${COMPILER_FAMILY}" = GCC ]; then
     CONFIG_OPTS+=("CXX=${CXX}")
     CONFIG_OPTS+=("CPP=${CPP}")
 fi
+# Clone and build dependencies, if not yet installed to Travis env as DEBs
+echo "`date`: Starting build of dependencies (if any)..."
 
 ###########   libsodium
-echo ""
-BASE_PWD=${PWD}
-echo "`date`: INFO: Building prerequisite 'libsodium' from Git repository..." >&2
-git clone --branch=${LIBSODIUM_VER} git://github.com/jedisct1/libsodium.git
-cd libsodium
+if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libsodium-dev >/dev/null 2>&1); then
+    echo ""
+    BASE_PWD=${PWD}
+    echo "`date`: INFO: Building prerequisite 'libsodium' from Git repository..." >&2
+    git clone --branch=${LIBSODIUM_VER} git://github.com/jedisct1/libsodium.git
+    cd libsodium
 
-# CCache directory
-CCACHE_BASEDIR=${PWD}
-export CCACHE_BASEDIR
+    # CCache directory
+    CCACHE_BASEDIR=${PWD}
+    export CCACHE_BASEDIR
 
-git --no-pager log --oneline -n1o
+    git --no-pager log --oneline -n1o
 
-# Build/Install
-if [ -e autogen.sh ]; then
-    ./autogen.sh 2> /dev/null
+    # Build/Install
+    if [ -e autogen.sh ]; then
+        ./autogen.sh 2> /dev/null
+    fi
+    if [ -e buildconf ]; then
+        ./buildconf 2> /dev/null
+    fi
+    if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
+        libtoolize --copy --force && \
+            aclocal -I . && \
+            autoheader && \
+            automake --add-missing --copy && \
+            autoconf || \
+            autoreconf -fiv
+    fi
+    ./configure "${CONFIG_OPTS[@]}"
+    make -j4
+    make install
+    cd "${BASE_PWD}"
 fi
-if [ -e buildconf ]; then
-    ./buildconf 2> /dev/null
-fi
-if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
-    libtoolize --copy --force && \
-        aclocal -I . && \
-        autoheader && \
-        automake --add-missing --copy && \
-        autoconf || \
-        autoreconf -fiv
-fi
-./configure "${CONFIG_OPTS[@]}"
-make -j4
-make install
-cd "${BASE_PWD}"
 
 ###########   libzmq
-echo ""
-BASE_PWD=${PWD}
-echo "`date`: INFO: Building prerequisite 'libzmq' from Git repository..." >&2
-git clone --branch=${LIBZMQ_VER} git://github.com/zeromq/libzmq.git
-cd libzmq
+if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libzmq3-dev >/dev/null 2>&1); then
+    echo ""
+    BASE_PWD=${PWD}
+    echo "`date`: INFO: Building prerequisite 'libzmq' from Git repository..." >&2
+    git clone --branch=${LIBZMQ_VER} git://github.com/zeromq/libzmq.git
+    cd libzmq
 
-# CCache directory
-CCACHE_BASEDIR=${PWD}
-export CCACHE_BASEDIR
+    # CCache directory
+    CCACHE_BASEDIR=${PWD}
+    export CCACHE_BASEDIR
 
-git --no-pager log --oneline -n1o
+    git --no-pager log --oneline -n1o
 
-# Build/Install
-if [ -e autogen.sh ]; then
-    ./autogen.sh 2> /dev/null
+    # Build/Install
+    if [ -e autogen.sh ]; then
+        ./autogen.sh 2> /dev/null
+    fi
+    if [ -e buildconf ]; then
+        ./buildconf 2> /dev/null
+    fi
+    if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
+        libtoolize --copy --force && \
+            aclocal -I . && \
+            autoheader && \
+            automake --add-missing --copy && \
+            autoconf || \
+            autoreconf -fiv
+    fi
+    ./configure "${CONFIG_OPTS[@]}" "${ZMQ_CONFIG_OPTS[@]}"
+    make -j4
+    make install
+    cd "${BASE_PWD}"
 fi
-if [ -e buildconf ]; then
-    ./buildconf 2> /dev/null
-fi
-if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
-    libtoolize --copy --force && \
-        aclocal -I . && \
-        autoheader && \
-        automake --add-missing --copy && \
-        autoconf || \
-        autoreconf -fiv
-fi
-./configure "${CONFIG_OPTS[@]}" "${ZMQ_CONFIG_OPTS[@]}"
-make -j4
-make install
-cd "${BASE_PWD}"
 
 ###########   CZMQ
-echo ""
-BASE_PWD=${PWD}
-echo "`date`: INFO: Building prerequisite 'czmq' from Git repository..." >&2
-git clone --branch=${LIBCZMQ_VER} git://github.com/zeromq/czmq.git
-cd czmq
+if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libczmq-dev >/dev/null 2>&1); then
+    echo ""
+    BASE_PWD=${PWD}
+    echo "`date`: INFO: Building prerequisite 'czmq' from Git repository..." >&2
+    git clone --branch=${LIBCZMQ_VER} git://github.com/zeromq/czmq.git
+    cd czmq
 
-# CCache directory
-CCACHE_BASEDIR=${PWD}
-export CCACHE_BASEDIR
+    # CCache directory
+    CCACHE_BASEDIR=${PWD}
+    export CCACHE_BASEDIR
 
-git --no-pager log --oneline -n1o
+    git --no-pager log --oneline -n1o
 
-# Build/Install
-if [ -e autogen.sh ]; then
-    ./autogen.sh 2> /dev/null
+    # Build/Install
+    if [ -e autogen.sh ]; then
+        ./autogen.sh 2> /dev/null
+    fi
+    if [ -e buildconf ]; then
+        ./buildconf 2> /dev/null
+    fi
+    if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
+        libtoolize --copy --force && \
+            aclocal -I . && \
+            autoheader && \
+            automake --add-missing --copy && \
+            autoconf || \
+            autoreconf -fiv
+    fi
+    ./configure "${CONFIG_OPTS[@]}"
+    make -j4
+    make install
+    cd "${BASE_PWD}"
 fi
-if [ -e buildconf ]; then
-    ./buildconf 2> /dev/null
-fi
-if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
-    libtoolize --copy --force && \
-        aclocal -I . && \
-        autoheader && \
-        automake --add-missing --copy && \
-        autoconf || \
-        autoreconf -fiv
-fi
-./configure "${CONFIG_OPTS[@]}"
-make -j4
-make install
-cd "${BASE_PWD}"
 
 ###########   Malamute
-echo ""
-BASE_PWD=${PWD}
-echo "`date`: INFO: Building prerequisite 'malamute' from Git repository..." >&2
-git clone --branch=${MALAMUTE_VER} git://github.com/lnls-dig/malamute.git
-cd malamute
+if ! (command -v dpkg-query >/dev/null 2>&1 && dpkg-query --list libmlm-dev >/dev/null 2>&1); then
+    echo ""
+    BASE_PWD=${PWD}
+    echo "`date`: INFO: Building prerequisite 'malamute' from Git repository..." >&2
+    git clone --branch=${MALAMUTE_VER} git://github.com/lnls-dig/malamute.git
+    cd malamute
 
-# CCache directory
-CCACHE_BASEDIR=${PWD}
-export CCACHE_BASEDIR
+    # CCache directory
+    CCACHE_BASEDIR=${PWD}
+    export CCACHE_BASEDIR
 
-git --no-pager log --oneline -n1o
+    git --no-pager log --oneline -n1o
 
-# Build/Install
-if [ -e autogen.sh ]; then
-    ./autogen.sh 2> /dev/null
+    # Build/Install
+    if [ -e autogen.sh ]; then
+        ./autogen.sh 2> /dev/null
+    fi
+    if [ -e buildconf ]; then
+        ./buildconf 2> /dev/null
+    fi
+    if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
+        libtoolize --copy --force && \
+            aclocal -I . && \
+            autoheader && \
+            automake --add-missing --copy && \
+            autoconf || \
+            autoreconf -fiv
+    fi
+    ./configure "${CONFIG_OPTS[@]}"
+    make -j4
+    make install
+    cd "${BASE_PWD}"
 fi
-if [ -e buildconf ]; then
-    ./buildconf 2> /dev/null
-fi
-if [ ! -e autogen.sh ] && [ ! -e buildconf ] && [ ! -e ./configure ] && [ -s ./configure.ac ]; then
-    libtoolize --copy --force && \
-        aclocal -I . && \
-        autoheader && \
-        automake --add-missing --copy && \
-        autoconf || \
-        autoreconf -fiv
-fi
-./configure "${CONFIG_OPTS[@]}"
-make -j4
-make install
-cd "${BASE_PWD}"
 
 ########## HALCS
 if [ "$PURE_MAKE" = yes ]; then
