578f4b0b208d0f98f8c1a73eee02bf22ecfbab75
Lucas Russo <lerwys@gmail.com>
2020-05-12 14:11:38 -0300

    ci_show_err_logs.sh: fix checking if dir exists before running "find"

--- ci_show_err_logs.sh
+++ ci_show_err_logs.sh
@@ -7,13 +7,15 @@ set -o
 
 if [ -n "$CPACK" ]; then
     # Show logs if present
-    CPACK_LOGS=$(find build/_CPack_Packages/Linux/DEB -iname "*.log")
-    for log in "${CPACK_LOGS}"; do
-        if [ -f "${log}" ]; then
-            echo "Showing log ${log}:"
-            echo "------------------------------------------------------"
-            cat ${log}
-            echo "------------------------------------------------------"
-        fi
-    done
+    if [ -d build/_CPack_Packages/Linux ]; then
+        CPACK_LOGS=$(find build/_CPack_Packages/Linux -iname "*.log")
+        for log in "${CPACK_LOGS}"; do
+            if [ -f "${log}" ]; then
+                echo "Showing log ${log}:"
+                echo "------------------------------------------------------"
+                cat ${log}
+                echo "------------------------------------------------------"
+            fi
+        done
+    fi
 fi
