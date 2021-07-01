5eae022286977cc83edf8b7d037a1d36a4947402
Lucas Russo <lerwys@gmail.com>
2021-07-01 20:03:10 -0300

    boards: remove last extern const symbols

--- include/boards/afcv3_1/ddr3_defs.h
+++ include/boards/afcv3_1/ddr3_defs.h
@@ -5,23 +5,10 @@
 
 #ifdef __SHRINK_AFCV3_DDR_SIZE__
 #define __MEM_TOTAL_SIZE                (1ULL << 28) /* 256 MB reserved for position storage */
-extern const size_t MEM_TOTAL_SIZE;
 #else
 #define __MEM_TOTAL_SIZE                (1ULL << 31) /* 2 GB reserved for position storage */
-extern const size_t MEM_TOTAL_SIZE;
 #endif
 
 #define  __MEM_REGION_SIZE              (__MEM_TOTAL_SIZE / 16)
-extern const size_t MEM_REGION_SIZE;
-
-/* FPGA Specific */
-extern const size_t DDR3_PAYLOAD_SIZE;        /* In Bytes: Artix7 (AFCv3) */
-
-/* DDR3 Specific */
-
-/* DDR3 for AFCv3 has a 32-bit interface */
-extern const size_t DDR3_DATA_WIDTH;           /* In Bits */
-extern const size_t DDR3_BYTE_2_BIT;
-extern const size_t DDR3_ADDR_WORD_2_BYTE;
 
 #endif
--- include/boards/ml605/ddr3_defs.h
+++ include/boards/ml605/ddr3_defs.h
@@ -4,19 +4,6 @@
 #include <stddef.h>
 
 #define __MEM_TOTAL_SIZE                (1ULL << 28) /* 256 MB reserved for position storage */
-extern const size_t MEM_TOTAL_SIZE;
-
 #define  __MEM_REGION_SIZE              (__MEM_TOTAL_SIZE / 8)
-extern const size_t MEM_REGION_SIZE;
-
-/* FPGA Specific */
-extern const size_t DDR3_PAYLOAD_SIZE;        /* In Bytes: Virtex6 (ML605) */
-
-/* DDR3 Specific */
-
-/* DDR3 for ML605 has a 64-bit interface */
-extern const size_t DDR3_DATA_WIDTH;           /* In Bits */
-extern const size_t DDR3_BYTE_2_BIT;
-extern const size_t DDR3_ADDR_WORD_2_BYTE;
 
 #endif
