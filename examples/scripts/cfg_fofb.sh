26fb0cba4a992f33f485f7f80b1de9246b518024
Lucas Russo <lerwys@gmail.com>
2021-07-22 15:56:56 -0300

    examples: add more control over number of DCCs in cfg_fofb.sh

--- examples/scripts/cfg_fofb.sh
+++ examples/scripts/cfg_fofb.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # This must follow the format "<port_number>,<bitstream_name_without_extension>"
 BOARDS="$1"
+DCCS="${2:-3}"
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
@@ -11,16 +12,14 @@ OLDIFS=$IFS; IFS=',';
 # Separate "tuple" arguments with positional notation
 set -- ${BOARDS};
 for board in ${BOARDS[*]}; do
-    printf "Configuring BPM board: %s\n" "${board}"
-    (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 0 --bpm_id ${board} --time_frame_len 10000 --cc_enable 0)
-    (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 1 --bpm_id ${board} --time_frame_len 10000 --cc_enable 0)
-    (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 2 --bpm_id ${board} --time_frame_len 10000 --cc_enable 0)
-    (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 0 --cc_enable 1)
-    (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 1 --cc_enable 1)
-    (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 2 --cc_enable 1)
-    (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 0 --channumber 2 --rcvsrc 0 --rcvsel 5)
-    (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 1 --channumber 2 --rcvsrc 0 --rcvsel 5)
-    (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber 2 --channumber 2 --rcvsrc 0 --rcvsel 5)
-done;
+    IFS=$OLDIFS;
 
-IFS=$OLDIFS;
+    printf "Configuring BPM board: %s\n" "${board}"
+    for dcc in `seq 0 $((${DCCS}-1))`; do
+        
+        printf "DCC: %s\n" "${dcc}"
+        (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --bpm_id ${board} --time_frame_len 5000 --cc_enable 0)
+        (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --cc_enable 1)
+        (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 2 --rcvsrc 0 --rcvsel 5)
+    done;
+done;
