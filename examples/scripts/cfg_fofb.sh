98f9d3ddd54c08bbd3c5117bd83bcaf9e2ed863b
Lucas Russo <lerwys@gmail.com>
2021-08-09 14:05:51 -0300

    examples/scripts: add FOFB compatibility with old/new gateware

--- examples/scripts/cfg_fofb.sh
+++ examples/scripts/cfg_fofb.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # This must follow the format "<port_number>,<bitstream_name_without_extension>"
 BOARDS="$1"
 DCCS="${2:-3}"
+NUM_GTS="${3:-8}"
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
@@ -16,10 +17,15 @@ for board in ${BOARDS[*]}; do
 
     printf "Configuring BPM board: %s\n" "${board}"
     for dcc in `seq 0 $((${DCCS}-1))`; do
+        num_gts=${NUM_GTS}
         
         printf "DCC: %s\n" "${dcc}"
-        (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --bpm_id ${board} --time_frame_len 5000 --cc_enable 0)
-        (cd build/ && ./examples/fofb_ctrl --verbose --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --cc_enable 1)
+        (cd build/ && ./examples/fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --bpm_id $((2*${board}-1)) --time_frame_len 5000 --cc_enable 0)
+
+        (cd build/ && ./examples/fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --cc_enable 1)
+        # old gateware
         (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 2 --rcvsrc 0 --rcvsel 5)
+        # new bpm gateware
+        (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 20 --rcvsrc 0 --rcvsel 5)
     done;
 done;
