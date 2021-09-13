dd13fa41fefeaf7bec6371e407f52cbc5fc13522
Lucas Russo <lerwys@gmail.com>
2021-09-13 16:11:02 -0300

    examples: add trigger config. for old AFC RTM SFP gateware design

--- examples/scripts/cfg_fofb.sh
+++ examples/scripts/cfg_fofb.sh
@@ -18,12 +18,13 @@ for board in ${BOARDS[*]}; do
     printf "Configuring BPM board: %s\n" "${board}"
     for dcc in `seq 0 $((${DCCS}-1))`; do
         num_gts=${NUM_GTS}
-        
+
         printf "DCC: %s\n" "${dcc}"
         (cd build/ && ./examples/fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --bpm_id $((2*${board}-1)) --time_frame_len 5000 --cc_enable 0)
 
         (cd build/ && ./examples/fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --cc_enable 1)
         # old gateware
+        (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 1 --rcvsrc 0 --rcvsel 5)
         (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 2 --rcvsrc 0 --rcvsel 5)
         # new bpm gateware
         (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 20 --rcvsrc 0 --rcvsel 5)
