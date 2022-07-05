619d53c015736e55a5b495af344169435792ae3d
Guilherme Ricioli <guilherme.riciolic@gmail.com>
2022-07-05 15:20:28 -0300

    Little tweaks
    * Set DCC IDs properly
    * Use installed halcs examples
    * Set triggers only for BPMs (FOFB doesn't use them)

--- examples/scripts/cfg_fofb.sh
+++ examples/scripts/cfg_fofb.sh
@@ -6,10 +6,14 @@ set -euo pipefail
 BOARDS="$1"
 DCCS="${2:-3}"
 NUM_GTS="${3:-8}"
+CRATE_NUM=$4
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 OLDIFS=$IFS; IFS=',';
+
+FOFB_CTRLS_OFFS=480
+
 # Separate "tuple" arguments with positional notation
 set -- ${BOARDS};
 for board in ${BOARDS[*]}; do
@@ -19,14 +23,24 @@ for board in ${BOARDS[*]}; do
     for dcc in `seq 0 $((${DCCS}-1))`; do
         num_gts=${NUM_GTS}
 
-        printf "DCC: %s\n" "${dcc}"
-        (cd build/ && ./examples/fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --bpm_id $((2*${board}-1)) --time_frame_len 5000 --cc_enable 0)
+        if [ ${board} -eq 2 ]; then
+          # FOFB Controllers' IDs range from 480 to 499
+          bpm_id=$((${FOFB_CTRLS_OFFS} + (${CRATE_NUM}-1)))
+        else
+          # BPMs' IDs range from 0 to 159
+          bpm_id=$((8*(${CRATE_NUM}-1) + (2*(${board}-7))))
+        fi
 
-        (cd build/ && ./examples/fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --cc_enable 1)
-        # old gateware
-        (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 1 --rcvsrc 0 --rcvsel 5)
-        (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 2 --rcvsrc 0 --rcvsel 5)
-        # new bpm gateware
-        (cd build/ && ./examples/trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 20 --rcvsrc 0 --rcvsel 5)
+        printf "DCC: %s\n" "${dcc}"
+        (fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --bpm_id ${bpm_id} --time_frame_len 5000 --cc_enable 0)
+        (fofb_ctrl --verbose --num_gts ${num_gts} --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --cc_enable 1)
+
+        if [ ${board} -ne 2 ]; then
+          # old bpm gateware
+          (trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 1 --rcvsrc 0 --rcvsel 5)
+          (trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 2 --rcvsrc 0 --rcvsel 5)
+          # new bpm gateware
+          (trigger --brokerendp ipc:///tmp/malamute --boardslot ${board} --halcsnumber ${dcc} --channumber 20 --rcvsrc 0 --rcvsel 5)
+        fi
     done;
 done;
